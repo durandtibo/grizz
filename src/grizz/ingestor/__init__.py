1cb16199cdfe04a94174799ecfcc72f5e422493e
Thibaut Durand <durand.tibo@gmail.com>
2024-07-06 14:03:19 -0700

    Add `CsvIngestor`

--- src/grizz/ingestor/__init__.py
+++ src/grizz/ingestor/__init__.py
@@ -5,9 +5,11 @@ from __future__ import annotations
 __all__ = [
     "BaseIngestor",
     "Ingestor",
+    "CsvIngestor",
     "is_ingestor_config",
     "setup_ingestor",
 ]
 
 from grizz.ingestor.base import BaseIngestor, is_ingestor_config, setup_ingestor
+from grizz.ingestor.csv import CsvIngestor
 from grizz.ingestor.vanilla import Ingestor
