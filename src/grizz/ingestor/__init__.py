8fb49d975303092e4406eedd910b93b95565f1e5
Thibaut Durand <durand.tibo@gmail.com>
2024-07-06 14:04:53 -0700

    Add `ParquetIngestor` (#11)

--- src/grizz/ingestor/__init__.py
+++ src/grizz/ingestor/__init__.py
@@ -4,12 +4,14 @@ from __future__ import annotations
 
 __all__ = [
     "BaseIngestor",
-    "Ingestor",
     "CsvIngestor",
+    "Ingestor",
+    "ParquetIngestor",
     "is_ingestor_config",
     "setup_ingestor",
 ]
 
 from grizz.ingestor.base import BaseIngestor, is_ingestor_config, setup_ingestor
 from grizz.ingestor.csv import CsvIngestor
+from grizz.ingestor.parquet import ParquetIngestor
 from grizz.ingestor.vanilla import Ingestor
