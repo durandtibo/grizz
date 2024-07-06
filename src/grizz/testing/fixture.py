a509345eb01fd92bee3ce55491c153defe150985
Thibaut Durand <durand.tibo@gmail.com>
2024-07-06 15:19:24 -0700

    Add `pyarrow` import utils (#20)

--- src/grizz/testing/fixture.py
+++ src/grizz/testing/fixture.py
@@ -2,13 +2,14 @@ r"""Define some utility functions for testing."""
 
 from __future__ import annotations
 
-__all__ = ["clickhouse_connect_available", "tqdm_available"]
+__all__ = ["clickhouse_connect_available", "pyarrow_available", "tqdm_available"]
 
 import pytest
 
 from grizz.utils.imports import is_clickhouse_connect_available, is_tqdm_available
 
 clickhouse_connect_available = pytest.mark.skipif(
-    not is_clickhouse_connect_available(), reason="Requires clickhouse_connect"
+    not is_clickhouse_connect_available(), reason="requires clickhouse_connect"
 )
-tqdm_available = pytest.mark.skipif(not is_tqdm_available(), reason="Requires tqdm")
+pyarrow_available = pytest.mark.skipif(not is_tqdm_available(), reason="requires pyarrow")
+tqdm_available = pytest.mark.skipif(not is_tqdm_available(), reason="requires tqdm")
