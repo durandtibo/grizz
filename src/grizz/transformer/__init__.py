feb1e171bd99a892865525bfbd915c7f1ccbca99
Thibaut Durand <durand.tibo@gmail.com>
2024-09-07 11:53:12 -0700

    Add `ConcatColumnsTransformer` (#147)

--- src/grizz/transformer/__init__.py
+++ src/grizz/transformer/__init__.py
@@ -9,6 +9,8 @@ __all__ = [
     "CastTransformer",
     "ColumnSelection",
     "ColumnSelectionTransformer",
+    "ConcatColumns",
+    "ConcatColumnsTransformer",
     "DecimalCast",
     "DecimalCastTransformer",
     "Diff",
@@ -70,6 +72,8 @@ from grizz.transformer.casting import FloatCastTransformer as FloatCast
 from grizz.transformer.casting import IntegerCastTransformer
 from grizz.transformer.casting import IntegerCastTransformer as IntegerCast
 from grizz.transformer.columns import BaseColumnsTransformer
+from grizz.transformer.concat import ConcatColumnsTransformer
+from grizz.transformer.concat import ConcatColumnsTransformer as ConcatColumns
 from grizz.transformer.datetime import ToDatetimeTransformer
 from grizz.transformer.datetime import ToDatetimeTransformer as ToDatetime
 from grizz.transformer.diff import DiffTransformer
---
+++ tests/unit/transformer/test_concat.py
@@ -0,0 +1,151 @@
+from __future__ import annotations
+
+import logging
+
+import polars as pl
+import pytest
+from polars.testing import assert_frame_equal
+
+from grizz.transformer import ConcatColumns
+
+
+@pytest.fixture
+def dataframe() -> pl.DataFrame:
+    return pl.DataFrame(
+        {
+            "col1": [11, 12, 13, 14, 15],
+            "col2": [21, 22, 23, 24, 25],
+            "col3": [31, 32, 33, 34, 35],
+            "col4": ["a", "b", "c", "d", "e"],
+        },
+        schema={"col1": pl.Int64, "col2": pl.Int64, "col3": pl.Int64, "col4": pl.String},
+    )
+
+
+##############################################
+#     Tests for ConcatColumnsTransformer     #
+##############################################
+
+
+def test_cast_transformer_repr() -> None:
+    assert repr(ConcatColumns(columns=["col1", "col3"], out_column="out")).startswith(
+        "ConcatColumnsTransformer("
+    )
+
+
+def test_cast_transformer_str() -> None:
+    assert str(ConcatColumns(columns=["col1", "col3"], out_column="out")).startswith(
+        "ConcatColumnsTransformer("
+    )
+
+
+def test_cast_transformer_transform_1_col(dataframe: pl.DataFrame) -> None:
+    transformer = ConcatColumns(columns=["col1"], out_column="out")
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [11, 12, 13, 14, 15],
+                "col2": [21, 22, 23, 24, 25],
+                "col3": [31, 32, 33, 34, 35],
+                "col4": ["a", "b", "c", "d", "e"],
+                "out": [[11], [12], [13], [14], [15]],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Int64,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "out": pl.List(pl.Int64),
+            },
+        ),
+    )
+
+
+def test_cast_transformer_transform_2_cols(dataframe: pl.DataFrame) -> None:
+    transformer = ConcatColumns(columns=["col1", "col3"], out_column="out")
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [11, 12, 13, 14, 15],
+                "col2": [21, 22, 23, 24, 25],
+                "col3": [31, 32, 33, 34, 35],
+                "col4": ["a", "b", "c", "d", "e"],
+                "out": [[11, 31], [12, 32], [13, 33], [14, 34], [15, 35]],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Int64,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "out": pl.List(pl.Int64),
+            },
+        ),
+    )
+
+
+def test_cast_transformer_transform_3_cols(dataframe: pl.DataFrame) -> None:
+    transformer = ConcatColumns(columns=["col1", "col2", "col3"], out_column="out")
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [11, 12, 13, 14, 15],
+                "col2": [21, 22, 23, 24, 25],
+                "col3": [31, 32, 33, 34, 35],
+                "col4": ["a", "b", "c", "d", "e"],
+                "out": [[11, 21, 31], [12, 22, 32], [13, 23, 33], [14, 24, 34], [15, 25, 35]],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Int64,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "out": pl.List(pl.Int64),
+            },
+        ),
+    )
+
+
+def test_cast_transformer_transform_ignore_missing_false(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = ConcatColumns(columns=["col1", "col3", "col5"], out_column="out")
+    with pytest.raises(RuntimeError, match="1 columns are missing in the DataFrame:"):
+        transformer.transform(dataframe)
+
+
+def test_cast_transformer_transform_ignore_missing_true(
+    dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = ConcatColumns(
+        columns=["col1", "col3", "col5"], out_column="out", ignore_missing=True
+    )
+    with caplog.at_level(logging.WARNING):
+        out = transformer.transform(dataframe)
+        assert_frame_equal(
+            out,
+            pl.DataFrame(
+                {
+                    "col1": [11, 12, 13, 14, 15],
+                    "col2": [21, 22, 23, 24, 25],
+                    "col3": [31, 32, 33, 34, 35],
+                    "col4": ["a", "b", "c", "d", "e"],
+                    "out": [[11, 31], [12, 32], [13, 33], [14, 34], [15, 35]],
+                },
+                schema={
+                    "col1": pl.Int64,
+                    "col2": pl.Int64,
+                    "col3": pl.Int64,
+                    "col4": pl.String,
+                    "out": pl.List(pl.Int64),
+                },
+            ),
+        )
+        assert caplog.messages[-1].startswith(
+            "1 columns are missing in the DataFrame and will be ignored:"
+        )
