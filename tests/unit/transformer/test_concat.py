3a6c244d8dbd8b423c8c9b0cabfd550b13774bba
Thibaut Durand <durand.tibo@gmail.com>
2024-12-21 13:43:56 -0800

    Improve `ConcatColumnsTransformer` (#243)

--- tests/unit/transformer/test_concat.py
+++ tests/unit/transformer/test_concat.py
@@ -27,19 +27,52 @@ def dataframe() -> pl.DataFrame:
 ##############################################
 
 
-def test_cast_transformer_repr() -> None:
+def test_concat_transformer_repr() -> None:
     assert repr(ConcatColumns(columns=["col1", "col3"], out_column="out")).startswith(
         "ConcatColumnsTransformer("
     )
 
 
-def test_cast_transformer_str() -> None:
+def test_concat_transformer_str() -> None:
     assert str(ConcatColumns(columns=["col1", "col3"], out_column="out")).startswith(
         "ConcatColumnsTransformer("
     )
 
 
-def test_cast_transformer_transform_1_col(dataframe: pl.DataFrame) -> None:
+def test_concat_transformer_fit(dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture) -> None:
+    transformer = ConcatColumns(columns=["col1", "col3"], out_column="out")
+    with caplog.at_level(logging.INFO):
+        transformer.fit(dataframe)
+    assert caplog.messages[0].startswith(
+        "Skipping 'ConcatColumnsTransformer.fit' as there are no parameters available to fit"
+    )
+
+
+def test_concat_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
+    transformer = ConcatColumns(columns=["col1", "col3"], out_column="out")
+    out = transformer.fit_transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [11, 12, 13, 14, 15],
+                "col2": [21, 22, 23, 24, 25],
+                "col3": [31, 32, 33, 34, 35],
+                "col4": ["a", "b", "c", "d", "e"],
+                "out": [[11, 31], [12, 32], [13, 33], [14, 34], [15, 35]],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Int64,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "out": pl.List(pl.Int64),
+            },
+        ),
+    )
+
+
+def test_concat_transformer_transform_1_col(dataframe: pl.DataFrame) -> None:
     transformer = ConcatColumns(columns=["col1"], out_column="out")
     out = transformer.transform(dataframe)
     assert_frame_equal(
@@ -63,7 +96,7 @@ def test_cast_transformer_transform_1_col(dataframe: pl.DataFrame) -> None:
     )
 
 
-def test_cast_transformer_transform_2_cols(dataframe: pl.DataFrame) -> None:
+def test_concat_transformer_transform_2_cols(dataframe: pl.DataFrame) -> None:
     transformer = ConcatColumns(columns=["col1", "col3"], out_column="out")
     out = transformer.transform(dataframe)
     assert_frame_equal(
@@ -87,7 +120,7 @@ def test_cast_transformer_transform_2_cols(dataframe: pl.DataFrame) -> None:
     )
 
 
-def test_cast_transformer_transform_3_cols(dataframe: pl.DataFrame) -> None:
+def test_concat_transformer_transform_3_cols(dataframe: pl.DataFrame) -> None:
     transformer = ConcatColumns(columns=["col1", "col2", "col3"], out_column="out")
     out = transformer.transform(dataframe)
     assert_frame_equal(
@@ -111,7 +144,7 @@ def test_cast_transformer_transform_3_cols(dataframe: pl.DataFrame) -> None:
     )
 
 
-def test_cast_transformer_transform_ignore_missing_false(
+def test_concat_transformer_transform_ignore_missing_false(
     dataframe: pl.DataFrame,
 ) -> None:
     transformer = ConcatColumns(columns=["col1", "col3", "col5"], out_column="out")
@@ -119,7 +152,7 @@ def test_cast_transformer_transform_ignore_missing_false(
         transformer.transform(dataframe)
 
 
-def test_cast_transformer_transform_ignore_missing_true(
+def test_concat_transformer_transform_ignore_missing_true(
     dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture
 ) -> None:
     transformer = ConcatColumns(
