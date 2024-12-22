7142d0684fcbe3ddb2823edecb449eaa7ab6933f
Thibaut Durand <durand.tibo@gmail.com>
2024-12-22 12:04:58 -0800

    Update `DropDuplicateTransformer` (#264)

--- src/grizz/transformer/duplicate.py
+++ src/grizz/transformer/duplicate.py
@@ -9,8 +9,9 @@ from typing import TYPE_CHECKING, Any
 
 import polars as pl
 import polars.selectors as cs
+from coola.utils.format import repr_mapping_line
 
-from grizz.transformer.columns2 import BaseColumnsTransformer
+from grizz.transformer.columns import BaseColumnsTransformer
 from grizz.utils.format import str_kwargs, str_row_diff
 
 if TYPE_CHECKING:
@@ -25,9 +26,14 @@ class DropDuplicateTransformer(BaseColumnsTransformer):
     Args:
         columns: The columns to check. If set to ``None`` (default),
             use all columns.
-        ignore_missing: If ``False``, an exception is raised if a
-            column is missing, otherwise just a warning message is
-            shown.
+        missing_policy: The policy on how to handle missing columns.
+            The following options are available: ``'ignore'``,
+            ``'warn'``, and ``'raise'``. If ``'raise'``, an exception
+            is raised if at least one column is missing.
+            If ``'warn'``, a warning is raised if at least one column
+            is missing and the missing columns are ignored.
+            If ``'ignore'``, the missing columns are ignored and
+            no message is shown.
         **kwargs: The keyword arguments for ``unique``.
 
     Example usage:
@@ -38,7 +44,7 @@ class DropDuplicateTransformer(BaseColumnsTransformer):
     >>> from grizz.transformer import DropDuplicate
     >>> transformer = DropDuplicate(keep="first", maintain_order=True)
     >>> transformer
-    DropDuplicateTransformer(columns=None, ignore_missing=False, keep=first, maintain_order=True)
+    DropDuplicateTransformer(columns=None, missing_policy='raise', keep=first, maintain_order=True)
     >>> frame = pl.DataFrame(
     ...     {
     ...         "col1": [1, 2, 3, 4, 1],
@@ -80,23 +86,28 @@ class DropDuplicateTransformer(BaseColumnsTransformer):
     def __init__(
         self,
         columns: Sequence[str] | None = None,
-        ignore_missing: bool = False,
+        missing_policy: str = "raise",
         **kwargs: Any,
     ) -> None:
-        super().__init__(columns, ignore_missing)
+        super().__init__(columns=columns, missing_policy=missing_policy)
         self._kwargs = kwargs
 
     def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__qualname__}(columns={self._columns}, "
-            f"ignore_missing={self._ignore_missing}{str_kwargs(self._kwargs)})"
+        args = repr_mapping_line({"columns": self._columns, "missing_policy": self._missing_policy})
+        return f"{self.__class__.__qualname__}({args}{str_kwargs(self._kwargs)})"
+
+    def fit(self, frame: pl.DataFrame) -> None:  # noqa: ARG002
+        logger.info(
+            f"Skipping '{self.__class__.__qualname__}.fit' as there are no parameters "
+            f"available to fit"
         )
 
-    def _pre_transform(self, frame: pl.DataFrame) -> None:
-        columns = self.find_common_columns(frame)
-        logger.info(f"Dropping duplicate rows by checking {len(columns):,} columns....")
-
-    def _transform(self, frame: pl.DataFrame) -> pl.DataFrame:
+    def transform(self, frame: pl.DataFrame) -> pl.DataFrame:
+        logger.info(
+            f"Dropping duplicate rows by checking {len(self.find_common_columns(frame)):,} "
+            "columns...."
+        )
+        self._check_missing_columns(frame)
         columns = self.find_common_columns(frame)
         initial_shape = frame.shape
         out = frame.unique(subset=cs.by_name(columns), **self._kwargs)
--- tests/unit/transformer/test_duplicate.py
+++ tests/unit/transformer/test_duplicate.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import logging
+import warnings
+
 import polars as pl
 import pytest
 from polars.testing import assert_frame_equal
@@ -25,22 +28,49 @@ def dataframe() -> pl.DataFrame:
 
 
 def test_drop_duplicate_transformer_repr() -> None:
-    assert repr(DropDuplicate()) == "DropDuplicateTransformer(columns=None, ignore_missing=False)"
+    assert repr(DropDuplicate()) == "DropDuplicateTransformer(columns=None, missing_policy='raise')"
 
 
 def test_drop_duplicate_transformer_repr_with_kwargs() -> None:
     assert repr(DropDuplicate(columns=["col1", "col3"], keep="first")) == (
-        "DropDuplicateTransformer(columns=('col1', 'col3'), ignore_missing=False, keep=first)"
+        "DropDuplicateTransformer(columns=('col1', 'col3'), missing_policy='raise', keep=first)"
     )
 
 
 def test_drop_duplicate_transformer_str() -> None:
-    assert str(DropDuplicate()) == "DropDuplicateTransformer(columns=None, ignore_missing=False)"
+    assert str(DropDuplicate()) == "DropDuplicateTransformer(columns=None, missing_policy='raise')"
 
 
 def test_drop_duplicate_transformer_str_with_kwargs() -> None:
     assert str(DropDuplicate(columns=["col1", "col3"], keep="first")) == (
-        "DropDuplicateTransformer(columns=('col1', 'col3'), ignore_missing=False, keep=first)"
+        "DropDuplicateTransformer(columns=('col1', 'col3'), missing_policy='raise', keep=first)"
+    )
+
+
+def test_drop_duplicate_transformer_fit(
+    dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = DropDuplicate(keep="first", maintain_order=True)
+    with caplog.at_level(logging.INFO):
+        transformer.fit(dataframe)
+    assert caplog.messages[0].startswith(
+        "Skipping 'DropDuplicateTransformer.fit' as there are no parameters available to fit"
+    )
+
+
+def test_drop_duplicate_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
+    transformer = DropDuplicate(keep="first", maintain_order=True)
+    out = transformer.fit_transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4],
+                "col2": ["1", "2", "3", "4"],
+                "col3": ["1", "2", "3", "1"],
+                "col4": ["a", "a", "a", "a"],
+            }
+        ),
     )
 
 
@@ -92,22 +122,44 @@ def test_drop_duplicate_transformer_transform_empty() -> None:
     assert_frame_equal(out, pl.DataFrame({}))
 
 
-def test_drop_duplicate_transformer_transform_columns_ignore_missing_false(
-    dataframe: pl.DataFrame,
-) -> None:
-    transformer = DropDuplicate(columns=["col1", "col2", "col5"])
-    with pytest.raises(ColumnNotFoundError, match="1 columns are missing in the DataFrame:"):
-        transformer.transform(dataframe)
-
-
-def test_drop_duplicate_transformer_transform_columns_ignore_missing_true(
+def test_drop_duplicate_transformer_transform_missing_policy_ignore(
     dataframe: pl.DataFrame,
 ) -> None:
     transformer = DropDuplicate(
-        columns=["col1", "col2", "col5"], ignore_missing=True, keep="first", maintain_order=True
+        columns=["col1", "col2", "col5"], keep="first", maintain_order=True, missing_policy="ignore"
+    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4],
+                "col2": ["1", "2", "3", "4"],
+                "col3": ["1", "2", "3", "1"],
+                "col4": ["a", "a", "a", "a"],
+            }
+        ),
+    )
+
+
+def test_drop_duplicate_transformer_transform_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = DropDuplicate(columns=["col1", "col2", "col5"])
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
+        transformer.transform(dataframe)
+
+
+def test_drop_duplicate_transformer_transform_missing_policy_warn(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = DropDuplicate(
+        columns=["col1", "col2", "col5"], keep="first", maintain_order=True, missing_policy="warn"
     )
     with pytest.warns(
-        ColumnNotFoundWarning, match="1 columns are missing in the DataFrame and will be ignored:"
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
     ):
         out = transformer.transform(dataframe)
     assert_frame_equal(
