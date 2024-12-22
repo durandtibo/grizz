acd5937d29e929ef84ff535605fd6e8a8099659a
Thibaut Durand <durand.tibo@gmail.com>
2024-12-22 11:49:48 -0800

    Update `FillNanTransformer` and `FillNullTransformer`

--- src/grizz/transformer/fill.py
+++ src/grizz/transformer/fill.py
@@ -9,8 +9,9 @@ from typing import TYPE_CHECKING, Any
 
 import polars as pl
 import polars.selectors as cs
+from coola.utils.format import repr_mapping_line
 
-from grizz.transformer.columns2 import BaseColumnsTransformer
+from grizz.transformer.columns import BaseColumnsTransformer
 from grizz.utils.format import str_kwargs
 
 if TYPE_CHECKING:
@@ -26,9 +27,14 @@ class FillNanTransformer(BaseColumnsTransformer):
     Args:
         columns: The columns to prepare. If ``None``, it processes all
             the columns of type string.
-        ignore_missing: If ``False``, an exception is raised if a
-            column is missing, otherwise just a warning message is
-            shown.
+        missing_policy: The policy on how to handle missing columns.
+            The following options are available: ``'ignore'``,
+            ``'warn'``, and ``'raise'``. If ``'raise'``, an exception
+            is raised if at least one column is missing.
+            If ``'warn'``, a warning is raised if at least one column
+            is missing and the missing columns are ignored.
+            If ``'ignore'``, the missing columns are ignored and
+            no message is shown.
         **kwargs: The keyword arguments for ``fill_nan``.
 
     Example usage:
@@ -39,7 +45,7 @@ class FillNanTransformer(BaseColumnsTransformer):
     >>> from grizz.transformer import FillNan
     >>> transformer = FillNan(columns=["col1", "col4"], value=100)
     >>> transformer
-    FillNanTransformer(columns=('col1', 'col4'), ignore_missing=False, value=100)
+    FillNanTransformer(columns=('col1', 'col4'), missing_policy='raise', value=100)
     >>> frame = pl.DataFrame(
     ...     {
     ...         "col1": [1, 2, 3, 4, None],
@@ -80,22 +86,24 @@ class FillNanTransformer(BaseColumnsTransformer):
     """
 
     def __init__(
-        self, columns: Sequence[str] | None = None, ignore_missing: bool = False, **kwargs: Any
+        self, columns: Sequence[str] | None = None, missing_policy: str = "raise", **kwargs: Any
     ) -> None:
-        super().__init__(columns=columns, ignore_missing=ignore_missing)
+        super().__init__(columns=columns, missing_policy=missing_policy)
         self._kwargs = kwargs
 
     def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__qualname__}(columns={self._columns}, "
-            f"ignore_missing={self._ignore_missing}{str_kwargs(self._kwargs)})"
+        args = repr_mapping_line({"columns": self._columns, "missing_policy": self._missing_policy})
+        return f"{self.__class__.__qualname__}({args}{str_kwargs(self._kwargs)})"
+
+    def fit(self, frame: pl.DataFrame) -> None:  # noqa: ARG002
+        logger.info(
+            f"Skipping '{self.__class__.__qualname__}.fit' as there are no parameters "
+            f"available to fit"
         )
 
-    def _pre_transform(self, frame: pl.DataFrame) -> None:
-        columns = self.find_columns(frame)
-        logger.info(f"Filling NaN values of {len(columns):,} columns...")
-
-    def _transform(self, frame: pl.DataFrame) -> pl.DataFrame:
+    def transform(self, frame: pl.DataFrame) -> pl.DataFrame:
+        logger.info(f"Filling NaN values of {len(self.find_columns(frame)):,} columns...")
+        self._check_missing_columns(frame)
         columns = self.find_common_columns(frame)
         return frame.with_columns(
             frame.select((cs.by_name(columns) & cs.float()).fill_nan(**self._kwargs))
@@ -108,9 +116,14 @@ class FillNullTransformer(BaseColumnsTransformer):
     Args:
         columns: The columns to prepare. If ``None``, it processes all
             the columns of type string.
-        ignore_missing: If ``False``, an exception is raised if a
-            column is missing, otherwise just a warning message is
-            shown.
+        missing_policy: The policy on how to handle missing columns.
+            The following options are available: ``'ignore'``,
+            ``'warn'``, and ``'raise'``. If ``'raise'``, an exception
+            is raised if at least one column is missing.
+            If ``'warn'``, a warning is raised if at least one column
+            is missing and the missing columns are ignored.
+            If ``'ignore'``, the missing columns are ignored and
+            no message is shown.
         **kwargs: The keyword arguments for ``fill_null``.
 
     Example usage:
@@ -121,7 +134,7 @@ class FillNullTransformer(BaseColumnsTransformer):
     >>> from grizz.transformer import FillNull
     >>> transformer = FillNull(columns=["col1", "col4"], value=100)
     >>> transformer
-    FillNullTransformer(columns=('col1', 'col4'), ignore_missing=False, value=100)
+    FillNullTransformer(columns=('col1', 'col4'), missing_policy='raise', value=100)
     >>> frame = pl.DataFrame(
     ...     {
     ...         "col1": [1, 2, 3, 4, None],
@@ -162,21 +175,23 @@ class FillNullTransformer(BaseColumnsTransformer):
     """
 
     def __init__(
-        self, columns: Sequence[str] | None = None, ignore_missing: bool = False, **kwargs: Any
+        self, columns: Sequence[str] | None = None, missing_policy: str = "raise", **kwargs: Any
     ) -> None:
-        super().__init__(columns=columns, ignore_missing=ignore_missing)
+        super().__init__(columns=columns, missing_policy=missing_policy)
         self._kwargs = kwargs
 
     def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__qualname__}(columns={self._columns}, "
-            f"ignore_missing={self._ignore_missing}{str_kwargs(self._kwargs)})"
+        args = repr_mapping_line({"columns": self._columns, "missing_policy": self._missing_policy})
+        return f"{self.__class__.__qualname__}({args}{str_kwargs(self._kwargs)})"
+
+    def fit(self, frame: pl.DataFrame) -> None:  # noqa: ARG002
+        logger.info(
+            f"Skipping '{self.__class__.__qualname__}.fit' as there are no parameters "
+            f"available to fit"
         )
 
-    def _pre_transform(self, frame: pl.DataFrame) -> None:
-        columns = self.find_columns(frame)
-        logger.info(f"Filling NaN values of {len(columns):,} columns...")
-
-    def _transform(self, frame: pl.DataFrame) -> pl.DataFrame:
+    def transform(self, frame: pl.DataFrame) -> pl.DataFrame:
+        logger.info(f"Filling NaN values of {len(self.find_columns(frame)):,} columns...")
+        self._check_missing_columns(frame)
         columns = self.find_common_columns(frame)
         return frame.with_columns(frame.select(cs.by_name(columns).fill_null(**self._kwargs)))
--- tests/unit/transformer/test_fill.py
+++ tests/unit/transformer/test_fill.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import logging
+import warnings
+
 import polars as pl
 import pytest
 from polars.testing import assert_frame_equal
@@ -33,25 +36,58 @@ def dataframe() -> pl.DataFrame:
 
 def test_fill_nan_transformer_repr() -> None:
     assert repr(FillNan(columns=["col1", "col4"])) == (
-        "FillNanTransformer(columns=('col1', 'col4'), ignore_missing=False)"
+        "FillNanTransformer(columns=('col1', 'col4'), missing_policy='raise')"
     )
 
 
 def test_fill_nan_transformer_repr_with_kwargs() -> None:
     assert repr(FillNan(columns=["col1", "col4"], value=100)) == (
-        "FillNanTransformer(columns=('col1', 'col4'), ignore_missing=False, value=100)"
+        "FillNanTransformer(columns=('col1', 'col4'), missing_policy='raise', value=100)"
     )
 
 
 def test_fill_nan_transformer_str() -> None:
     assert str(FillNan(columns=["col1", "col4"])) == (
-        "FillNanTransformer(columns=('col1', 'col4'), ignore_missing=False)"
+        "FillNanTransformer(columns=('col1', 'col4'), missing_policy='raise')"
     )
 
 
 def test_fill_nan_transformer_str_with_kwargs() -> None:
     assert str(FillNan(columns=["col1", "col4"], value=100)) == (
-        "FillNanTransformer(columns=('col1', 'col4'), ignore_missing=False, value=100)"
+        "FillNanTransformer(columns=('col1', 'col4'), missing_policy='raise', value=100)"
+    )
+
+
+def test_fill_nan_transformer_transformer_fit(
+    dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = FillNan(columns=["col1", "col4"], value=100)
+    with caplog.at_level(logging.INFO):
+        transformer.fit(dataframe)
+    assert caplog.messages[0].startswith(
+        "Skipping 'FillNanTransformer.fit' as there are no parameters available to fit"
+    )
+
+
+def test_fill_nan_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
+    transformer = FillNan(columns=["col1", "col4"], value=100)
+    out = transformer.fit_transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, None],
+                "col2": [1.2, 2.2, 3.2, 4.2, float("nan")],
+                "col3": ["a", "b", "c", "d", None],
+                "col4": [1.2, 100.0, 3.2, None, 5.2],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Float64,
+                "col3": pl.String,
+                "col4": pl.Float64,
+            },
+        ),
     )
 
 
@@ -112,18 +148,42 @@ def test_fill_nan_transformer_transform_empty_row() -> None:
     assert_frame_equal(out, pl.DataFrame({"col": []}, schema={"col": pl.String}))
 
 
-def test_fill_nan_transformer_transform_ignore_missing_false(
+def test_fill_nan_transformer_transform_missing_policy_ignore(dataframe: pl.DataFrame) -> None:
+    transformer = FillNan(columns=["col1", "col4", "col5"], missing_policy="ignore", value=100)
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, None],
+                "col2": [1.2, 2.2, 3.2, 4.2, float("nan")],
+                "col3": ["a", "b", "c", "d", None],
+                "col4": [1.2, 100.0, 3.2, None, 5.2],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Float64,
+                "col3": pl.String,
+                "col4": pl.Float64,
+            },
+        ),
+    )
+
+
+def test_fill_nan_transformer_transform_missing_policy_raise(
     dataframe: pl.DataFrame,
 ) -> None:
     transformer = FillNan(columns=["col2", "col3", "col5"], value=100)
-    with pytest.raises(ColumnNotFoundError, match="1 columns are missing in the DataFrame:"):
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
         transformer.transform(dataframe)
 
 
-def test_fill_nan_transformer_transform_ignore_missing_true(dataframe: pl.DataFrame) -> None:
-    transformer = FillNan(columns=["col1", "col4", "col5"], ignore_missing=True, value=100)
+def test_fill_nan_transformer_transform_missing_policy_warn(dataframe: pl.DataFrame) -> None:
+    transformer = FillNan(columns=["col1", "col4", "col5"], missing_policy="warn", value=100)
     with pytest.warns(
-        ColumnNotFoundWarning, match="1 columns are missing in the DataFrame and will be ignored:"
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
     ):
         out = transformer.transform(dataframe)
     assert_frame_equal(
@@ -182,25 +242,58 @@ def test_fill_nan_transformer_find_missing_columns_none(dataframe: pl.DataFrame)
 
 def test_fill_null_transformer_repr() -> None:
     assert repr(FillNull(columns=["col1", "col4"])) == (
-        "FillNullTransformer(columns=('col1', 'col4'), ignore_missing=False)"
+        "FillNullTransformer(columns=('col1', 'col4'), missing_policy='raise')"
     )
 
 
 def test_fill_null_transformer_repr_with_kwargs() -> None:
     assert repr(FillNull(columns=["col1", "col4"], value=100)) == (
-        "FillNullTransformer(columns=('col1', 'col4'), ignore_missing=False, value=100)"
+        "FillNullTransformer(columns=('col1', 'col4'), missing_policy='raise', value=100)"
     )
 
 
 def test_fill_null_transformer_str() -> None:
     assert str(FillNull(columns=["col1", "col4"])) == (
-        "FillNullTransformer(columns=('col1', 'col4'), ignore_missing=False)"
+        "FillNullTransformer(columns=('col1', 'col4'), missing_policy='raise')"
     )
 
 
 def test_fill_null_transformer_str_with_kwargs() -> None:
     assert str(FillNull(columns=["col1", "col4"], value=100)) == (
-        "FillNullTransformer(columns=('col1', 'col4'), ignore_missing=False, value=100)"
+        "FillNullTransformer(columns=('col1', 'col4'), missing_policy='raise', value=100)"
+    )
+
+
+def test_fill_null_transformer_transformer_fit(
+    dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = FillNull(columns=["col1", "col4"], value=100)
+    with caplog.at_level(logging.INFO):
+        transformer.fit(dataframe)
+    assert caplog.messages[0].startswith(
+        "Skipping 'FillNullTransformer.fit' as there are no parameters available to fit"
+    )
+
+
+def test_fill_null_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
+    transformer = FillNull(columns=["col1", "col4"], value=100)
+    out = transformer.fit_transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 100],
+                "col2": [1.2, 2.2, 3.2, 4.2, float("nan")],
+                "col3": ["a", "b", "c", "d", None],
+                "col4": [1.2, float("nan"), 3.2, 100.0, 5.2],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Float64,
+                "col3": pl.String,
+                "col4": pl.Float64,
+            },
+        ),
     )
 
 
@@ -259,18 +352,42 @@ def test_fill_null_transformer_transform_empty_row() -> None:
     assert_frame_equal(out, pl.DataFrame({"col": []}, schema={"col": pl.String}))
 
 
-def test_fill_null_transformer_transform_ignore_missing_false(
+def test_fill_null_transformer_transform_missing_policy_ignore(dataframe: pl.DataFrame) -> None:
+    transformer = FillNull(columns=["col1", "col4", "col5"], missing_policy="ignore", value=100)
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 100],
+                "col2": [1.2, 2.2, 3.2, 4.2, float("nan")],
+                "col3": ["a", "b", "c", "d", None],
+                "col4": [1.2, float("nan"), 3.2, 100.0, 5.2],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Float64,
+                "col3": pl.String,
+                "col4": pl.Float64,
+            },
+        ),
+    )
+
+
+def test_fill_null_transformer_transform_missing_policy_raise(
     dataframe: pl.DataFrame,
 ) -> None:
     transformer = FillNull(columns=["col2", "col3", "col5"], value=100)
-    with pytest.raises(ColumnNotFoundError, match="1 columns are missing in the DataFrame:"):
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
         transformer.transform(dataframe)
 
 
-def test_fill_null_transformer_transform_ignore_missing_true(dataframe: pl.DataFrame) -> None:
-    transformer = FillNull(columns=["col1", "col4", "col5"], ignore_missing=True, value=100)
+def test_fill_null_transformer_transform_missing_policy_warn(dataframe: pl.DataFrame) -> None:
+    transformer = FillNull(columns=["col1", "col4", "col5"], missing_policy="warn", value=100)
     with pytest.warns(
-        ColumnNotFoundWarning, match="1 columns are missing in the DataFrame and will be ignored:"
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
     ):
         out = transformer.transform(dataframe)
     assert_frame_equal(
