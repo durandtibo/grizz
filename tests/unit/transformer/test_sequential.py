d7d0910736244d943266403051148260d2c6bfba
Thibaut Durand <durand.tibo@gmail.com>
2024-12-22 14:44:31 -0800

    Update `SequentialTransformer` (#276)

--- tests/unit/transformer/test_sequential.py
+++ tests/unit/transformer/test_sequential.py
@@ -1,9 +1,24 @@
 from __future__ import annotations
 
+import logging
+
 import polars as pl
+import pytest
 from polars.testing import assert_frame_equal
 
 from grizz.transformer import Cast, Sequential
+
+
+@pytest.fixture
+def dataframe() -> pl.DataFrame:
+    return pl.DataFrame(
+        {
+            "col1": [1, 2, 3, 4, 5],
+            "col2": ["1", "2", "3", "4", "5"],
+            "col3": ["a ", " b", "  c  ", "d", "e"],
+        }
+    )
+
 
 ###########################################
 #     Tests for SequentialTransformer     #
@@ -40,16 +55,39 @@ def test_sequential_transformer_str_empty() -> None:
     assert str(Sequential([])) == "SequentialTransformer()"
 
 
-def test_sequential_transformer_transform_1() -> None:
-    frame = pl.DataFrame(
-        {
-            "col1": [1, 2, 3, 4, 5],
-            "col2": ["1", "2", "3", "4", "5"],
-            "col3": ["a ", " b", "  c  ", "d", "e"],
-        }
-    )
+def test_sequential_transformer_fit_1(
+    caplog: pytest.LogCaptureFixture, dataframe: pl.DataFrame
+) -> None:
     transformer = Sequential([Cast(columns=["col1"], dtype=pl.Float32)])
-    out = transformer.transform(frame)
+    with caplog.at_level(logging.INFO):
+        transformer.fit(dataframe)
+    assert caplog.messages[0].startswith(
+        "Skipping 'CastTransformer.fit' as there are no parameters available to fit"
+    )
+
+
+def test_sequential_transformer_fit_2(
+    caplog: pytest.LogCaptureFixture, dataframe: pl.DataFrame
+) -> None:
+    transformer = Sequential(
+        [
+            Cast(columns=["col1"], dtype=pl.Float32),
+            Cast(columns=["col2"], dtype=pl.Int64),
+        ]
+    )
+    with caplog.at_level(logging.INFO):
+        transformer.fit(dataframe)
+    assert caplog.messages[0].startswith(
+        "Skipping 'CastTransformer.fit' as there are no parameters available to fit"
+    )
+    assert caplog.messages[1].startswith(
+        "Skipping 'CastTransformer.fit' as there are no parameters available to fit"
+    )
+
+
+def test_sequential_transformer_fit_transform_1(dataframe: pl.DataFrame) -> None:
+    transformer = Sequential([Cast(columns=["col1"], dtype=pl.Float32)])
+    out = transformer.fit_transform(dataframe)
     assert_frame_equal(
         out,
         pl.DataFrame(
@@ -63,21 +101,51 @@ def test_sequential_transformer_transform_1() -> None:
     )
 
 
-def test_sequential_transformer_transform_2() -> None:
-    frame = pl.DataFrame(
-        {
-            "col1": [1, 2, 3, 4, 5],
-            "col2": ["1", "2", "3", "4", "5"],
-            "col3": ["a ", " b", "  c  ", "d", "e"],
-        }
-    )
+def test_sequential_transformer_fit_transform_2(dataframe: pl.DataFrame) -> None:
     transformer = Sequential(
         [
             Cast(columns=["col1"], dtype=pl.Float32),
             Cast(columns=["col2"], dtype=pl.Int64),
         ]
     )
-    out = transformer.transform(frame)
+    out = transformer.fit_transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1.0, 2.0, 3.0, 4.0, 5.0],
+                "col2": [1, 2, 3, 4, 5],
+                "col3": ["a ", " b", "  c  ", "d", "e"],
+            },
+            schema={"col1": pl.Float32, "col2": pl.Int64, "col3": pl.String},
+        ),
+    )
+
+
+def test_sequential_transformer_transform_1(dataframe: pl.DataFrame) -> None:
+    transformer = Sequential([Cast(columns=["col1"], dtype=pl.Float32)])
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1.0, 2.0, 3.0, 4.0, 5.0],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": ["a ", " b", "  c  ", "d", "e"],
+            },
+            schema={"col1": pl.Float32, "col2": pl.String, "col3": pl.String},
+        ),
+    )
+
+
+def test_sequential_transformer_transform_2(dataframe: pl.DataFrame) -> None:
+    transformer = Sequential(
+        [
+            Cast(columns=["col1"], dtype=pl.Float32),
+            Cast(columns=["col2"], dtype=pl.Int64),
+        ]
+    )
+    out = transformer.transform(dataframe)
     assert_frame_equal(
         out,
         pl.DataFrame(
