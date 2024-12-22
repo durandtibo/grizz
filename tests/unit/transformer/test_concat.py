4a3dc7bf5f35e06ca82baf44c27084127de843fc
Thibaut Durand <durand.tibo@gmail.com>
2024-12-22 13:06:44 -0800

    Update `ConcatColumnsTransformer` (#268)

--- tests/unit/transformer/test_concat.py
+++ tests/unit/transformer/test_concat.py
@@ -1,12 +1,18 @@
 from __future__ import annotations
 
 import logging
+import warnings
 
 import polars as pl
 import pytest
 from polars.testing import assert_frame_equal
 
-from grizz.exceptions import ColumnNotFoundError, ColumnNotFoundWarning
+from grizz.exceptions import (
+    ColumnExistsError,
+    ColumnExistsWarning,
+    ColumnNotFoundError,
+    ColumnNotFoundWarning,
+)
 from grizz.transformer import ConcatColumns
 
 
@@ -29,19 +35,19 @@ def dataframe() -> pl.DataFrame:
 
 
 def test_concat_transformer_repr() -> None:
-    assert repr(ConcatColumns(columns=["col1", "col3"], out_column="out")).startswith(
+    assert repr(ConcatColumns(columns=["col1", "col3"], out_col="out")).startswith(
         "ConcatColumnsTransformer("
     )
 
 
 def test_concat_transformer_str() -> None:
-    assert str(ConcatColumns(columns=["col1", "col3"], out_column="out")).startswith(
+    assert str(ConcatColumns(columns=["col1", "col3"], out_col="out")).startswith(
         "ConcatColumnsTransformer("
     )
 
 
 def test_concat_transformer_fit(dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture) -> None:
-    transformer = ConcatColumns(columns=["col1", "col3"], out_column="out")
+    transformer = ConcatColumns(columns=["col1", "col3"], out_col="out")
     with caplog.at_level(logging.INFO):
         transformer.fit(dataframe)
     assert caplog.messages[0].startswith(
@@ -50,7 +56,7 @@ def test_concat_transformer_fit(dataframe: pl.DataFrame, caplog: pytest.LogCaptu
 
 
 def test_concat_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
-    transformer = ConcatColumns(columns=["col1", "col3"], out_column="out")
+    transformer = ConcatColumns(columns=["col1", "col3"], out_col="out")
     out = transformer.fit_transform(dataframe)
     assert_frame_equal(
         out,
@@ -74,7 +80,7 @@ def test_concat_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
 
 
 def test_concat_transformer_transform_1_col(dataframe: pl.DataFrame) -> None:
-    transformer = ConcatColumns(columns=["col1"], out_column="out")
+    transformer = ConcatColumns(columns=["col1"], out_col="out")
     out = transformer.transform(dataframe)
     assert_frame_equal(
         out,
@@ -98,7 +104,7 @@ def test_concat_transformer_transform_1_col(dataframe: pl.DataFrame) -> None:
 
 
 def test_concat_transformer_transform_2_cols(dataframe: pl.DataFrame) -> None:
-    transformer = ConcatColumns(columns=["col1", "col3"], out_column="out")
+    transformer = ConcatColumns(columns=["col1", "col3"], out_col="out")
     out = transformer.transform(dataframe)
     assert_frame_equal(
         out,
@@ -122,7 +128,7 @@ def test_concat_transformer_transform_2_cols(dataframe: pl.DataFrame) -> None:
 
 
 def test_concat_transformer_transform_3_cols(dataframe: pl.DataFrame) -> None:
-    transformer = ConcatColumns(columns=["col1", "col2", "col3"], out_column="out")
+    transformer = ConcatColumns(columns=["col1", "col2", "col3"], out_col="out")
     out = transformer.transform(dataframe)
     assert_frame_equal(
         out,
@@ -145,20 +151,106 @@ def test_concat_transformer_transform_3_cols(dataframe: pl.DataFrame) -> None:
     )
 
 
-def test_concat_transformer_transform_ignore_missing_false(
+def test_concat_transformer_transform_exist_policy_ignore(dataframe: pl.DataFrame) -> None:
+    transformer = ConcatColumns(columns=["col1", "col3"], out_col="col2", exist_policy="ignore")
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [11, 12, 13, 14, 15],
+                "col2": [[11, 31], [12, 32], [13, 33], [14, 34], [15, 35]],
+                "col3": [31, 32, 33, 34, 35],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.List(pl.Int64),
+                "col3": pl.Int64,
+                "col4": pl.String,
+            },
+        ),
+    )
+
+
+def test_concat_transformer_transform_exist_policy_raise(
     dataframe: pl.DataFrame,
 ) -> None:
-    transformer = ConcatColumns(columns=["col1", "col3", "col5"], out_column="out")
-    with pytest.raises(ColumnNotFoundError, match="1 columns are missing in the DataFrame:"):
+    transformer = ConcatColumns(columns=["col1", "col3"], out_col="col2")
+    with pytest.raises(ColumnExistsError, match="1 column already exists in the DataFrame:"):
         transformer.transform(dataframe)
 
 
-def test_concat_transformer_transform_ignore_missing_true(dataframe: pl.DataFrame) -> None:
+def test_concat_transformer_transform_exist_policy_warn(dataframe: pl.DataFrame) -> None:
+    transformer = ConcatColumns(columns=["col1", "col3"], out_col="col2", exist_policy="warn")
+    with pytest.warns(
+        ColumnExistsWarning,
+        match="1 column already exists in the DataFrame and will be overwritten:",
+    ):
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [11, 12, 13, 14, 15],
+                "col2": [[11, 31], [12, 32], [13, 33], [14, 34], [15, 35]],
+                "col3": [31, 32, 33, 34, 35],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.List(pl.Int64),
+                "col3": pl.Int64,
+                "col4": pl.String,
+            },
+        ),
+    )
+
+
+def test_concat_transformer_transform_missing_policy_ignore(dataframe: pl.DataFrame) -> None:
     transformer = ConcatColumns(
-        columns=["col1", "col3", "col5"], out_column="out", ignore_missing=True
+        columns=["col1", "col3", "col5"], out_col="out", missing_policy="ignore"
+    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [11, 12, 13, 14, 15],
+                "col2": [21, 22, 23, 24, 25],
+                "col3": [31, 32, 33, 34, 35],
+                "col4": ["a", "b", "c", "d", "e"],
+                "out": [[11, 31], [12, 32], [13, 33], [14, 34], [15, 35]],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Int64,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "out": pl.List(pl.Int64),
+            },
+        ),
+    )
+
+
+def test_concat_transformer_transform_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = ConcatColumns(columns=["col1", "col3", "col5"], out_col="out")
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
+        transformer.transform(dataframe)
+
+
+def test_concat_transformer_transform_missing_policy_warn(dataframe: pl.DataFrame) -> None:
+    transformer = ConcatColumns(
+        columns=["col1", "col3", "col5"], out_col="out", missing_policy="warn"
     )
     with pytest.warns(
-        ColumnNotFoundWarning, match="1 columns are missing in the DataFrame and will be ignored:"
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
     ):
         out = transformer.transform(dataframe)
     assert_frame_equal(
