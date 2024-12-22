cb13d0ef00ca579620e47f3a5850674076d919e9
Thibaut Durand <durand.tibo@gmail.com>
2024-12-22 12:47:26 -0800

    Update `ToDatetimeTransformer` (#266)

--- tests/unit/transformer/test_datetime.py
+++ tests/unit/transformer/test_datetime.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import warnings
 from datetime import datetime, timedelta, timezone
 
 import polars as pl
@@ -93,26 +94,26 @@ def dataframe() -> pl.DataFrame:
 
 def test_to_datetime_transformer_repr() -> None:
     assert repr(ToDatetime(columns=["col1", "col3"])) == (
-        "ToDatetimeTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False)"
+        "ToDatetimeTransformer(columns=('col1', 'col3'), format=None, missing_policy='raise')"
     )
 
 
 def test_to_datetime_transformer_repr_with_kwargs() -> None:
     assert repr(ToDatetime(columns=["col1", "col3"], strict=False)) == (
-        "ToDatetimeTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False, "
+        "ToDatetimeTransformer(columns=('col1', 'col3'), format=None, missing_policy='raise', "
         "strict=False)"
     )
 
 
 def test_to_datetime_transformer_str() -> None:
     assert str(ToDatetime(columns=["col1", "col3"])) == (
-        "ToDatetimeTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False)"
+        "ToDatetimeTransformer(columns=('col1', 'col3'), format=None, missing_policy='raise')"
     )
 
 
 def test_to_datetime_transformer_str_with_kwargs() -> None:
     assert str(ToDatetime(columns=["col1", "col3"], strict=False)) == (
-        "ToDatetimeTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False, "
+        "ToDatetimeTransformer(columns=('col1', 'col3'), format=None, missing_policy='raise', "
         "strict=False)"
     )
 
@@ -488,16 +489,138 @@ def test_to_datetime_transformer_transform_format(dataframe: pl.DataFrame) -> No
     )
 
 
-def test_to_datetime_transformer_transform_ignore_missing_false(dataframe: pl.DataFrame) -> None:
+def test_to_datetime_transformer_transform_missing_policy_ignore(dataframe: pl.DataFrame) -> None:
+    transformer = ToDatetime(columns=["col1", "col3", "col5"], missing_policy="ignore")
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [
+                    datetime(
+                        year=2020,
+                        month=1,
+                        day=1,
+                        hour=1,
+                        minute=1,
+                        second=1,
+                        tzinfo=timezone(timedelta(0)),
+                    ),
+                    datetime(
+                        year=2020,
+                        month=1,
+                        day=1,
+                        hour=2,
+                        minute=2,
+                        second=2,
+                        tzinfo=timezone(timedelta(0)),
+                    ),
+                    datetime(
+                        year=2020,
+                        month=1,
+                        day=1,
+                        hour=12,
+                        minute=0,
+                        second=1,
+                        tzinfo=timezone(timedelta(0)),
+                    ),
+                    datetime(
+                        year=2020,
+                        month=1,
+                        day=1,
+                        hour=18,
+                        minute=18,
+                        second=18,
+                        tzinfo=timezone(timedelta(0)),
+                    ),
+                    datetime(
+                        year=2020,
+                        month=1,
+                        day=1,
+                        hour=23,
+                        minute=59,
+                        second=59,
+                        tzinfo=timezone(timedelta(0)),
+                    ),
+                ],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [
+                    datetime(
+                        year=2020,
+                        month=1,
+                        day=1,
+                        hour=11,
+                        minute=11,
+                        second=11,
+                        tzinfo=timezone(timedelta(0)),
+                    ),
+                    datetime(
+                        year=2020,
+                        month=2,
+                        day=1,
+                        hour=12,
+                        minute=12,
+                        second=12,
+                        tzinfo=timezone(timedelta(0)),
+                    ),
+                    datetime(
+                        year=2020,
+                        month=3,
+                        day=1,
+                        hour=13,
+                        minute=13,
+                        second=13,
+                        tzinfo=timezone(timedelta(0)),
+                    ),
+                    datetime(
+                        year=2020,
+                        month=4,
+                        day=1,
+                        hour=8,
+                        minute=8,
+                        second=8,
+                        tzinfo=timezone(timedelta(0)),
+                    ),
+                    datetime(
+                        year=2020,
+                        month=5,
+                        day=1,
+                        hour=23,
+                        minute=59,
+                        second=59,
+                        tzinfo=timezone(timedelta(0)),
+                    ),
+                ],
+                "col4": [
+                    "2020-01-01 01:01:01",
+                    "2020-01-01 02:02:02",
+                    "2020-01-01 12:00:01",
+                    "2020-01-01 18:18:18",
+                    "2020-01-01 23:59:59",
+                ],
+            },
+            schema={
+                "col1": pl.Datetime(time_unit="us"),
+                "col2": pl.String,
+                "col3": pl.Datetime(time_unit="us"),
+                "col4": pl.String,
+            },
+        ),
+    )
+
+
+def test_to_datetime_transformer_transform_missing_policy_raise(dataframe: pl.DataFrame) -> None:
     transformer = ToDatetime(columns=["col1", "col3", "col5"])
-    with pytest.raises(ColumnNotFoundError, match="1 columns are missing in the DataFrame:"):
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
         transformer.transform(dataframe)
 
 
-def test_to_datetime_transformer_transform_ignore_missing_true(dataframe: pl.DataFrame) -> None:
-    transformer = ToDatetime(columns=["col1", "col3", "col5"], ignore_missing=True)
+def test_to_datetime_transformer_transform_missing_policy_warn(dataframe: pl.DataFrame) -> None:
+    transformer = ToDatetime(columns=["col1", "col3", "col5"], missing_policy="warn")
     with pytest.warns(
-        ColumnNotFoundWarning, match="1 columns are missing in the DataFrame and will be ignored:"
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
     ):
         out = transformer.transform(dataframe)
     assert_frame_equal(
