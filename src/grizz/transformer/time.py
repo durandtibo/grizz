a13d2192ea1712c8764bef7607e36d2500fefb1d
Thibaut Durand <durand.tibo@gmail.com>
2024-12-22 08:49:45 -0800

    Update `ToTimeTransformer`

--- src/grizz/transformer/time.py
+++ src/grizz/transformer/time.py
@@ -10,9 +10,10 @@ from typing import TYPE_CHECKING, Any
 
 import polars as pl
 import polars.selectors as cs
+from coola.utils.format import repr_mapping_line
 
 from grizz.transformer.base import BaseTransformer
-from grizz.transformer.columns2 import BaseColumnsTransformer
+from grizz.transformer.columns import BaseColumnsTransformer
 from grizz.utils.format import str_kwargs
 
 if TYPE_CHECKING:
@@ -116,9 +117,14 @@ class ToTimeTransformer(BaseColumnsTransformer):
             for the full specification. Example: ``"%H:%M:%S"``.
             If set to ``None`` (default), the format is inferred from
             the data.
-        ignore_missing: If ``False``, an exception is raised if a
-            column is missing, otherwise just a warning message is
-            shown.
+        missing_policy: The policy on how to handle missing columns.
+            The following options are available: ``'ignore'``,
+            ``'warn'``, and ``'raise'``. If ``'raise'``, an exception
+            is raised if at least one column is missing.
+            If ``'warn'``, a warning is raised if at least one column
+            is missing and the missing columns are ignored.
+            If ``'ignore'``, the missing columns are ignored and
+            no warning message is shown.
         **kwargs: The keyword arguments for ``to_time``.
 
     Example usage:
@@ -129,7 +135,7 @@ class ToTimeTransformer(BaseColumnsTransformer):
     >>> from grizz.transformer import ToTime
     >>> transformer = ToTime(columns=["col1"], format="%H:%M:%S")
     >>> transformer
-    ToTimeTransformer(columns=('col1',), format=%H:%M:%S, ignore_missing=False)
+    ToTimeTransformer(columns=('col1',), format='%H:%M:%S', missing_policy='raise')
     >>> frame = pl.DataFrame(
     ...     {
     ...         "col1": ["01:01:01", "02:02:02", "12:00:01", "18:18:18", "23:59:59"],
@@ -172,24 +178,34 @@ class ToTimeTransformer(BaseColumnsTransformer):
         self,
         columns: Sequence[str] | None,
         format: str | None = None,  # noqa: A002
-        ignore_missing: bool = False,
+        missing_policy: str = "raise",
         **kwargs: Any,
     ) -> None:
-        super().__init__(columns, ignore_missing)
+        super().__init__(columns=columns, missing_policy=missing_policy)
         self._format = format
         self._kwargs = kwargs
 
     def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__qualname__}(columns={self._columns}, format={self._format}, "
-            f"ignore_missing={self._ignore_missing}{str_kwargs(self._kwargs)})"
+        args = repr_mapping_line(
+            {
+                "columns": self._columns,
+                "format": self._format,
+                "missing_policy": self._missing_policy,
+            }
+        )
+        return f"{self.__class__.__qualname__}({args}{str_kwargs(self._kwargs)})"
+
+    def fit(self, frame: pl.DataFrame) -> None:  # noqa: ARG002
+        logger.info(
+            f"Skipping '{self.__class__.__qualname__}.fit' as there are no parameters "
+            f"available to fit"
         )
 
-    def _pre_transform(self, frame: pl.DataFrame) -> None:
-        columns = self.find_columns(frame)
-        logger.info(f"Converting {len(columns):,} columns to time ({self._format})...")
-
-    def _transform(self, frame: pl.DataFrame) -> pl.DataFrame:
+    def transform(self, frame: pl.DataFrame) -> pl.DataFrame:
+        logger.info(
+            f"Converting {len(self.find_columns(frame)):,} columns to time ({self._format})..."
+        )
+        self._check_missing_columns(frame)
         columns = self.find_common_columns(frame)
         return frame.with_columns(
             frame.select(
--- tests/unit/transformer/test_time.py
+++ tests/unit/transformer/test_time.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import datetime
+import logging
+import warnings
 
 import polars as pl
 import pytest
@@ -85,27 +87,67 @@ def frame_time() -> pl.DataFrame:
 
 def test_to_time_transformer_repr() -> None:
     assert repr(ToTime(columns=["col1", "col3"])) == (
-        "ToTimeTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False)"
+        "ToTimeTransformer(columns=('col1', 'col3'), format=None, missing_policy='raise')"
     )
 
 
 def test_to_time_transformer_repr_with_kwargs() -> None:
     assert repr(ToTime(columns=["col1", "col3"], strict=False)) == (
-        "ToTimeTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False, "
+        "ToTimeTransformer(columns=('col1', 'col3'), format=None, missing_policy='raise', "
         "strict=False)"
     )
 
 
 def test_to_time_transformer_str() -> None:
     assert str(ToTime(columns=["col1", "col3"])) == (
-        "ToTimeTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False)"
+        "ToTimeTransformer(columns=('col1', 'col3'), format=None, missing_policy='raise')"
     )
 
 
 def test_to_time_transformer_str_with_kwargs() -> None:
     assert str(ToTime(columns=["col1", "col3"], strict=False)) == (
-        "ToTimeTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False, "
+        "ToTimeTransformer(columns=('col1', 'col3'), format=None, missing_policy='raise', "
         "strict=False)"
+    )
+
+
+def test_to_time_transformer_fit(
+    frame_time: pl.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = ToTime(columns=["col1", "col3"])
+    with caplog.at_level(logging.INFO):
+        transformer.fit(frame_time)
+    assert caplog.messages[0].startswith(
+        "Skipping 'ToTimeTransformer.fit' as there are no parameters available to fit"
+    )
+
+
+def test_to_time_transformer_fit_transform(frame_time: pl.DataFrame) -> None:
+    transformer = ToTime(columns=["col1", "col3"])
+    out = transformer.fit_transform(frame_time)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [
+                    datetime.time(1, 1, 1),
+                    datetime.time(2, 2, 2),
+                    datetime.time(12, 0, 1),
+                    datetime.time(18, 18, 18),
+                    datetime.time(23, 59, 59),
+                ],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [
+                    datetime.time(11, 11, 11),
+                    datetime.time(12, 12, 12),
+                    datetime.time(13, 13, 13),
+                    datetime.time(8, 8, 8),
+                    datetime.time(23, 59, 59),
+                ],
+                "col4": ["01:01:01", "02:02:02", "12:00:01", "18:18:18", "23:59:59"],
+            },
+            schema={"col1": pl.Time, "col2": pl.String, "col3": pl.Time, "col4": pl.String},
+        ),
     )
 
 
@@ -167,16 +209,49 @@ def test_to_time_transformer_transform_format(frame_time: pl.DataFrame) -> None:
     )
 
 
-def test_to_time_transformer_transform_ignore_missing_false(frame_time: pl.DataFrame) -> None:
+def test_to_time_transformer_transform_missing_policy_ignore(frame_time: pl.DataFrame) -> None:
+    transformer = ToTime(
+        columns=["col1", "col3", "col5"], format="%H:%M:%S", missing_policy="ignore"
+    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(frame_time)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [
+                    datetime.time(hour=1, minute=1, second=1),
+                    datetime.time(hour=2, minute=2, second=2),
+                    datetime.time(hour=12, minute=0, second=1),
+                    datetime.time(hour=18, minute=18, second=18),
+                    datetime.time(hour=23, minute=59, second=59),
+                ],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [
+                    datetime.time(hour=11, minute=11, second=11),
+                    datetime.time(hour=12, minute=12, second=12),
+                    datetime.time(hour=13, minute=13, second=13),
+                    datetime.time(hour=8, minute=8, second=8),
+                    datetime.time(hour=23, minute=59, second=59),
+                ],
+                "col4": ["01:01:01", "02:02:02", "12:00:01", "18:18:18", "23:59:59"],
+            },
+            schema={"col1": pl.Time, "col2": pl.String, "col3": pl.Time, "col4": pl.String},
+        ),
+    )
+
+
+def test_to_time_transformer_transform_missing_policy_raise(frame_time: pl.DataFrame) -> None:
     transformer = ToTime(columns=["col1", "col3", "col5"], format="%H:%M:%S")
-    with pytest.raises(ColumnNotFoundError, match="1 columns are missing in the DataFrame:"):
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
         transformer.transform(frame_time)
 
 
-def test_to_time_transformer_transform_ignore_missing_true(frame_time: pl.DataFrame) -> None:
-    transformer = ToTime(columns=["col1", "col3", "col5"], format="%H:%M:%S", ignore_missing=True)
+def test_to_time_transformer_transform_missing_policy_warn(frame_time: pl.DataFrame) -> None:
+    transformer = ToTime(columns=["col1", "col3", "col5"], format="%H:%M:%S", missing_policy="warn")
     with pytest.warns(
-        ColumnNotFoundWarning, match="1 columns are missing in the DataFrame and will be ignored:"
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
     ):
         out = transformer.transform(frame_time)
     assert_frame_equal(
