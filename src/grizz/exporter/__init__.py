8fecf5b7b93751b61b0e738fbbe1c547229a0c43
Thibaut Durand <durand.tibo@gmail.com>
2024-12-24 07:51:40 -0800

    Add `CsvExporter` (#307)

--- src/grizz/exporter/__init__.py
+++ src/grizz/exporter/__init__.py
@@ -4,10 +4,12 @@ from __future__ import annotations
 
 __all__ = [
     "BaseExporter",
+    "CsvExporter",
     "ParquetExporter",
     "is_exporter_config",
     "setup_exporter",
 ]
 
 from grizz.exporter.base import BaseExporter, is_exporter_config, setup_exporter
+from grizz.exporter.csv import CsvExporter
 from grizz.exporter.parquet import ParquetExporter
