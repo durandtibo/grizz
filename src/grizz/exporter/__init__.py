4d4bb061139856690fb995b39a918b7c0bb22010
Thibaut Durand <durand.tibo@gmail.com>
2024-12-24 08:47:52 -0800

    Add `InMemoryExporter`

--- src/grizz/exporter/__init__.py
+++ src/grizz/exporter/__init__.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 __all__ = [
     "BaseExporter",
     "CsvExporter",
+    "InMemoryExporter",
     "ParquetExporter",
     "TransformExporter",
     "is_exporter_config",
@@ -13,5 +14,6 @@ __all__ = [
 
 from grizz.exporter.base import BaseExporter, is_exporter_config, setup_exporter
 from grizz.exporter.csv import CsvExporter
+from grizz.exporter.in_memory import InMemoryExporter
 from grizz.exporter.parquet import ParquetExporter
 from grizz.exporter.transform import TransformExporter
