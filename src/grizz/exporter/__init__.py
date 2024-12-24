dce44dc18aeaa91ab8ea4b8e7b868415abf73410
Thibaut Durand <durand.tibo@gmail.com>
2024-12-24 07:44:23 -0800

    Add `ParquetExporter` (#306)

--- src/grizz/exporter/__init__.py
+++ src/grizz/exporter/__init__.py
@@ -2,6 +2,12 @@ r"""Contain DataFrame exporters."""
 
 from __future__ import annotations
 
-__all__ = ["BaseExporter", "is_exporter_config", "setup_exporter"]
+__all__ = [
+    "BaseExporter",
+    "ParquetExporter",
+    "is_exporter_config",
+    "setup_exporter",
+]
 
 from grizz.exporter.base import BaseExporter, is_exporter_config, setup_exporter
+from grizz.exporter.parquet import ParquetExporter
