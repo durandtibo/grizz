8040262ec0f1c7a053c21232c0f71b584234006b
Thibaut Durand <durand.tibo@gmail.com>
2024-12-26 00:02:02 -0800

    Update `JsonDecodeTransformer`

--- tests/unit/transformer/test_json.py
+++ tests/unit/transformer/test_json.py
@@ -63,6 +63,27 @@ def test_json_decode_transformer_fit(
     )
 
 
+def test_json_decode_transformer_fit_missing_policy_ignore(dataframe: pl.DataFrame) -> None:
+    transformer = JsonDecode(columns=["list", "dict", "missing"], missing_policy="ignore")
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        transformer.fit(dataframe)
+
+
+def test_json_decode_transformer_fit_missing_policy_raise(dataframe: pl.DataFrame) -> None:
+    transformer = JsonDecode(columns=["list", "dict", "missing"])
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
+        transformer.fit(dataframe)
+
+
+def test_json_decode_transformer_fit_missing_policy_warn(dataframe: pl.DataFrame) -> None:
+    transformer = JsonDecode(columns=["list", "dict", "missing"], missing_policy="warn")
+    with pytest.warns(
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
+    ):
+        transformer.fit(dataframe)
+
+
 def test_json_decode_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
     transformer = JsonDecode(columns=["list", "dict"])
     out = transformer.fit_transform(dataframe)
