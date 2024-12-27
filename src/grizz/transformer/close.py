3fa9709bbc7ffc3eb411a0df56dab39862382185
Thibaut Durand <durand.tibo@gmail.com>
2024-12-27 09:43:49 -0800

    Improve `CloseColumnsTransformer`

--- src/grizz/transformer/close.py
+++ src/grizz/transformer/close.py
@@ -158,5 +158,5 @@ class CloseColumnsTransformer(BaseIn2Out1Transformer):
         if self._equal_nan:
             nan_check = frame[self._in1_col].is_nan() & frame[self._in2_col].is_nan()
             tol_check = tol_check | nan_check
-        logger.info(f"{self._out_col} - {str_boolean_series_stats(tol_check)}")
+        logger.info(f"column: {self._out_col!r} | {str_boolean_series_stats(tol_check)}")
         return frame.with_columns(tol_check.alias(self._out_col))
