67e1d591b46c7e49e26ed69497b28ec76ae7bef1
Thibaut Durand <durand.tibo@gmail.com>
2024-12-26 16:07:04 -0800

    Add `is_colorlog_available` (#370)

--- tests/unit/utils/test_imports.py
+++ tests/unit/utils/test_imports.py
@@ -6,11 +6,14 @@ import pytest
 
 from grizz.utils.imports import (
     check_clickhouse_connect,
+    check_colorlog,
     check_pyarrow,
     check_sklearn,
     check_tqdm,
     clickhouse_connect_available,
+    colorlog_available,
     is_clickhouse_connect_available,
+    is_colorlog_available,
     is_pyarrow_available,
     is_sklearn_available,
     is_tqdm_available,
@@ -74,6 +77,60 @@ def test_clickhouse_connect_available_decorator_without_package() -> None:
     with patch("grizz.utils.imports.is_clickhouse_connect_available", lambda: False):
 
         @clickhouse_connect_available
+        def fn(n: int = 0) -> int:
+            return 42 + n
+
+        assert fn(2) is None
+
+
+####################
+#     colorlog     #
+####################
+
+
+def test_check_colorlog_with_package() -> None:
+    with patch("grizz.utils.imports.is_colorlog_available", lambda: True):
+        check_colorlog()
+
+
+def test_check_colorlog_without_package() -> None:
+    with (
+        patch("grizz.utils.imports.is_colorlog_available", lambda: False),
+        pytest.raises(RuntimeError, match="'colorlog' package is required but not installed."),
+    ):
+        check_colorlog()
+
+
+def test_is_colorlog_available() -> None:
+    assert isinstance(is_colorlog_available(), bool)
+
+
+def test_colorlog_available_with_package() -> None:
+    with patch("grizz.utils.imports.is_colorlog_available", lambda: True):
+        fn = colorlog_available(my_function)
+        assert fn(2) == 44
+
+
+def test_colorlog_available_without_package() -> None:
+    with patch("grizz.utils.imports.is_colorlog_available", lambda: False):
+        fn = colorlog_available(my_function)
+        assert fn(2) is None
+
+
+def test_colorlog_available_decorator_with_package() -> None:
+    with patch("grizz.utils.imports.is_colorlog_available", lambda: True):
+
+        @colorlog_available
+        def fn(n: int = 0) -> int:
+            return 42 + n
+
+        assert fn(2) == 44
+
+
+def test_colorlog_available_decorator_without_package() -> None:
+    with patch("grizz.utils.imports.is_colorlog_available", lambda: False):
+
+        @colorlog_available
         def fn(n: int = 0) -> int:
             return 42 + n
 
