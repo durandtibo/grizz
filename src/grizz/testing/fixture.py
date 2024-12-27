329ea69ebac02dae20fb1d1ec1399009da904103
Thibaut Durand <durand.tibo@gmail.com>
2024-12-26 16:08:50 -0800

    Add `configure_logging`

--- src/grizz/testing/fixture.py
+++ src/grizz/testing/fixture.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 __all__ = [
     "clickhouse_connect_available",
+    "colorlog_available",
     "pyarrow_available",
     "sklearn_available",
     "tqdm_available",
@@ -13,6 +14,7 @@ import pytest
 
 from grizz.utils.imports import (
     is_clickhouse_connect_available,
+    is_colorlog_available,
     is_pyarrow_available,
     is_sklearn_available,
     is_tqdm_available,
@@ -21,6 +23,7 @@ from grizz.utils.imports import (
 clickhouse_connect_available = pytest.mark.skipif(
     not is_clickhouse_connect_available(), reason="requires clickhouse_connect"
 )
+colorlog_available = pytest.mark.skipif(not is_colorlog_available(), reason="requires colorlog")
 pyarrow_available = pytest.mark.skipif(not is_pyarrow_available(), reason="requires pyarrow")
 sklearn_available = pytest.mark.skipif(not is_sklearn_available(), reason="requires sklearn")
 tqdm_available = pytest.mark.skipif(not is_tqdm_available(), reason="requires tqdm")
