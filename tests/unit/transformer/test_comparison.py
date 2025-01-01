58abab343be8785a5c53df5df7d417d33cb1577c
Thibaut Durand <durand.tibo@gmail.com>
2025-01-01 11:41:11 -0800

    Add `EqualMissingTransformer` (#426)

--- tests/unit/transformer/test_comparison.py
+++ tests/unit/transformer/test_comparison.py
@@ -15,6 +15,7 @@ from grizz.exceptions import (
 )
 from grizz.transformer import (
     Equal,
+    EqualMissing,
     Greater,
     GreaterEqual,
     Lower,
@@ -321,6 +322,325 @@ def test_equal_transformer_transform_missing_policy_warn(
     dataframe: pl.DataFrame,
 ) -> None:
     transformer = Equal(
+        columns=["col1", "col3", "col5"],
+        target=3,
+        prefix="",
+        suffix="_ind",
+        missing_policy="warn",
+    )
+    with pytest.warns(
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
+    ):
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [10, 20, 30, 40, 50],
+                "col4": ["a", "b", "c", "d", "e"],
+                "col1_ind": [False, False, True, False, False],
+                "col3_ind": [False, False, False, False, False],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Float32,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "col1_ind": pl.Boolean,
+                "col3_ind": pl.Boolean,
+            },
+        ),
+    )
+
+
+#############################################
+#     Tests for EqualMissingTransformer     #
+#############################################
+
+
+def test_equal_missing_transformer_repr() -> None:
+    assert repr(EqualMissing(columns=["col1", "col3"], target=3, prefix="", suffix="_ind")) == (
+        "EqualMissingTransformer(columns=('col1', 'col3'), target=3, prefix='', suffix='_ind', "
+        "exclude_columns=(), exist_policy='raise', missing_policy='raise')"
+    )
+
+
+def test_equal_missing_transformer_str() -> None:
+    assert str(EqualMissing(columns=["col1", "col3"], target=3, prefix="", suffix="_ind")) == (
+        "EqualMissingTransformer(columns=('col1', 'col3'), target=3, prefix='', suffix='_ind', "
+        "exclude_columns=(), exist_policy='raise', missing_policy='raise')"
+    )
+
+
+def test_equal_missing_transformer_fit(
+    dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = EqualMissing(columns=["col1", "col3"], target=3, prefix="", suffix="_ind")
+    with caplog.at_level(logging.INFO):
+        transformer.fit(dataframe)
+    assert caplog.messages[0].startswith(
+        "Skipping 'EqualMissingTransformer.fit' as there are no parameters available to fit"
+    )
+
+
+def test_equal_missing_transformer_fit_missing_policy_ignore(dataframe: pl.DataFrame) -> None:
+    transformer = EqualMissing(
+        columns=["col1", "col3", "col5"],
+        target=3,
+        prefix="",
+        suffix="_ind",
+        missing_policy="ignore",
+    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        transformer.fit(dataframe)
+
+
+def test_equal_missing_transformer_fit_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = EqualMissing(columns=["col1", "col3", "col5"], target=3, prefix="", suffix="_ind")
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
+        transformer.fit(dataframe)
+
+
+def test_equal_missing_transformer_fit_missing_policy_warn(dataframe: pl.DataFrame) -> None:
+    transformer = EqualMissing(
+        columns=["col1", "col3", "col5"],
+        target=3,
+        prefix="",
+        suffix="_ind",
+        missing_policy="warn",
+    )
+    with pytest.warns(
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
+    ):
+        transformer.fit(dataframe)
+
+
+def test_equal_missing_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
+    transformer = EqualMissing(columns=["col1", "col3"], target=3, prefix="", suffix="_ind")
+    out = transformer.fit_transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [10, 20, 30, 40, 50],
+                "col4": ["a", "b", "c", "d", "e"],
+                "col1_ind": [False, False, True, False, False],
+                "col3_ind": [False, False, False, False, False],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Float32,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "col1_ind": pl.Boolean,
+                "col3_ind": pl.Boolean,
+            },
+        ),
+    )
+
+
+def test_equal_missing_transformer_transform(dataframe: pl.DataFrame) -> None:
+    transformer = EqualMissing(columns=["col1", "col3"], target=3, prefix="", suffix="_ind")
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [10, 20, 30, 40, 50],
+                "col4": ["a", "b", "c", "d", "e"],
+                "col1_ind": [False, False, True, False, False],
+                "col3_ind": [False, False, False, False, False],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Float32,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "col1_ind": pl.Boolean,
+                "col3_ind": pl.Boolean,
+            },
+        ),
+    )
+
+
+def test_equal_missing_transformer_transform_target_2(dataframe: pl.DataFrame) -> None:
+    transformer = EqualMissing(columns=["col1", "col3"], target=2, prefix="", suffix="_ind")
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [10, 20, 30, 40, 50],
+                "col4": ["a", "b", "c", "d", "e"],
+                "col1_ind": [False, True, False, False, False],
+                "col3_ind": [False, False, False, False, False],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Float32,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "col1_ind": pl.Boolean,
+                "col3_ind": pl.Boolean,
+            },
+        ),
+    )
+
+
+def test_equal_missing_transformer_transform_nulls_and_nans() -> None:
+    frame = pl.DataFrame(
+        {
+            "col1": [1.0, None, 1.0, None, float("nan"), -1.0, float("nan")],
+            "col2": [-1.0, 1.0, None, None, -1.0, float("nan"), float("nan")],
+        },
+        schema={"col1": pl.Float32, "col2": pl.Float32},
+    )
+    transformer = EqualMissing(columns=["col1", "col2"], target=None, prefix="", suffix="_ind")
+    out = transformer.transform(frame)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1.0, None, 1.0, None, float("nan"), -1.0, float("nan")],
+                "col2": [-1.0, 1.0, None, None, -1.0, float("nan"), float("nan")],
+                "col1_ind": [False, True, False, True, False, False, False],
+                "col2_ind": [False, False, True, True, False, False, False],
+            },
+            schema={
+                "col1": pl.Float32,
+                "col2": pl.Float32,
+                "col1_ind": pl.Boolean,
+                "col2_ind": pl.Boolean,
+            },
+        ),
+    )
+
+
+def test_equal_missing_transformer_transform_exist_policy_ignore(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = EqualMissing(
+        columns=["col1", "col3"], target=3, prefix="", suffix="", exist_policy="ignore"
+    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [False, False, True, False, False],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [False, False, False, False, False],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={
+                "col1": pl.Boolean,
+                "col2": pl.Float32,
+                "col3": pl.Boolean,
+                "col4": pl.String,
+            },
+        ),
+    )
+
+
+def test_equal_missing_transformer_transform_exist_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = EqualMissing(columns=["col1", "col3"], target=3, prefix="", suffix="")
+    with pytest.raises(ColumnExistsError, match="2 columns already exist in the DataFrame:"):
+        transformer.transform(dataframe)
+
+
+def test_equal_missing_transformer_transform_exist_policy_warn(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = EqualMissing(
+        columns=["col1", "col3"], target=3, prefix="", suffix="", exist_policy="warn"
+    )
+    with pytest.warns(
+        ColumnExistsWarning,
+        match="2 columns already exist in the DataFrame and will be overwritten:",
+    ):
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [False, False, True, False, False],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [False, False, False, False, False],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={
+                "col1": pl.Boolean,
+                "col2": pl.Float32,
+                "col3": pl.Boolean,
+                "col4": pl.String,
+            },
+        ),
+    )
+
+
+def test_equal_missing_transformer_transform_missing_policy_ignore(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = EqualMissing(
+        columns=["col1", "col3", "col5"],
+        target=3,
+        prefix="",
+        suffix="_ind",
+        missing_policy="ignore",
+    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [10, 20, 30, 40, 50],
+                "col4": ["a", "b", "c", "d", "e"],
+                "col1_ind": [False, False, True, False, False],
+                "col3_ind": [False, False, False, False, False],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Float32,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "col1_ind": pl.Boolean,
+                "col3_ind": pl.Boolean,
+            },
+        ),
+    )
+
+
+def test_equal_missing_transformer_transform_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = EqualMissing(columns=["col1", "col3", "col5"], target=3, prefix="", suffix="_ind")
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
+        transformer.transform(dataframe)
+
+
+def test_equal_missing_transformer_transform_missing_policy_warn(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = EqualMissing(
         columns=["col1", "col3", "col5"],
         target=3,
         prefix="",
