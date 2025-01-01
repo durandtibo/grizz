6fbcaddd490420e1593c411043f1a84ee2660b2a
Thibaut Durand <durand.tibo@gmail.com>
2025-01-01 13:47:46 -0800

    Add `ColumnLowerTransformer`

--- tests/unit/transformer/test_column_comparison.py
+++ tests/unit/transformer/test_column_comparison.py
@@ -18,6 +18,7 @@ from grizz.transformer import (
     ColumnEqualMissing,
     ColumnGreater,
     ColumnGreaterEqual,
+    ColumnLower,
     ColumnLowerEqual,
 )
 
@@ -1143,6 +1144,227 @@ def test_column_lower_equal_transformer_transform_missing_policy_warn(
     dataframe: pl.DataFrame,
 ) -> None:
     transformer = ColumnLowerEqual(
+        in1_col="col",
+        in2_col="col2",
+        out_col="out",
+        missing_policy="warn",
+    )
+    with pytest.warns(
+        ColumnNotFoundWarning, match="column 'col' is missing in the DataFrame and will be ignored"
+    ):
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [5, 4, 3, 2, 1],
+                "col3": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Int64, "col2": pl.Int64, "col3": pl.String},
+        ),
+    )
+
+
+############################################
+#     Tests for ColumnLowerTransformer     #
+############################################
+
+
+def test_column_lower_transformer_repr() -> None:
+    assert repr(ColumnLower(in1_col="col1", in2_col="col2", out_col="out")) == (
+        "ColumnLowerTransformer(in1_col='col1', in2_col='col2', out_col='out', "
+        "exist_policy='raise', missing_policy='raise')"
+    )
+
+
+def test_column_lower_transformer_str() -> None:
+    assert str(ColumnLower(in1_col="col1", in2_col="col2", out_col="out")) == (
+        "ColumnLowerTransformer(in1_col='col1', in2_col='col2', out_col='out', "
+        "exist_policy='raise', missing_policy='raise')"
+    )
+
+
+def test_column_lower_transformer_fit(
+    dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = ColumnLower(in1_col="col1", in2_col="col2", out_col="out")
+    with caplog.at_level(logging.INFO):
+        transformer.fit(dataframe)
+    assert caplog.messages[0].startswith(
+        "Skipping 'ColumnLowerTransformer.fit' as there are no parameters available to fit"
+    )
+
+
+def test_column_lower_transformer_fit_missing_policy_ignore(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = ColumnLower(in1_col="col", in2_col="col2", out_col="out", missing_policy="ignore")
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        transformer.fit(dataframe)
+
+
+def test_column_lower_transformer_fit_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = ColumnLower(in1_col="col", in2_col="col2", out_col="out")
+    with pytest.raises(ColumnNotFoundError, match="column 'col' is missing in the DataFrame"):
+        transformer.fit(dataframe)
+
+
+def test_column_lower_transformer_fit_missing_policy_warn(dataframe: pl.DataFrame) -> None:
+    transformer = ColumnLower(in1_col="col", in2_col="col2", out_col="out", missing_policy="warn")
+    with pytest.warns(
+        ColumnNotFoundWarning, match="column 'col' is missing in the DataFrame and will be ignored"
+    ):
+        transformer.fit(dataframe)
+
+
+def test_column_lower_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
+    transformer = ColumnLower(in1_col="col1", in2_col="col2", out_col="out")
+    out = transformer.fit_transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [5, 4, 3, 2, 1],
+                "col3": ["a", "b", "c", "d", "e"],
+                "out": [True, True, False, False, False],
+            },
+            schema={"col1": pl.Int64, "col2": pl.Int64, "col3": pl.String, "out": pl.Boolean},
+        ),
+    )
+
+
+def test_column_lower_transformer_transform(dataframe: pl.DataFrame) -> None:
+    transformer = ColumnLower(in1_col="col1", in2_col="col2", out_col="out")
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [5, 4, 3, 2, 1],
+                "col3": ["a", "b", "c", "d", "e"],
+                "out": [True, True, False, False, False],
+            },
+            schema={"col1": pl.Int64, "col2": pl.Int64, "col3": pl.String, "out": pl.Boolean},
+        ),
+    )
+
+
+def test_column_lower_transformer_transform_nulls_and_nans() -> None:
+    frame = pl.DataFrame(
+        {
+            "col1": [1.0, None, 1.0, None, float("nan"), -1.0, float("nan")],
+            "col2": [1.0, 1.0, None, None, -1.0, float("nan"), float("nan")],
+        },
+        schema={"col1": pl.Float32, "col2": pl.Float32},
+    )
+    transformer = ColumnLower(in1_col="col1", in2_col="col2", out_col="out")
+    out = transformer.transform(frame)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1.0, None, 1.0, None, float("nan"), -1.0, float("nan")],
+                "col2": [1.0, 1.0, None, None, -1.0, float("nan"), float("nan")],
+                "out": [False, None, None, None, False, True, False],
+            },
+            schema={"col1": pl.Float32, "col2": pl.Float32, "out": pl.Boolean},
+        ),
+    )
+
+
+def test_column_lower_transformer_transform_exist_policy_ignore(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = ColumnLower(in1_col="col1", in2_col="col2", out_col="col3", exist_policy="ignore")
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [5, 4, 3, 2, 1],
+                "col3": [True, True, False, False, False],
+            },
+            schema={"col1": pl.Int64, "col2": pl.Int64, "col3": pl.Boolean},
+        ),
+    )
+
+
+def test_column_lower_transformer_transform_exist_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = ColumnLower(in1_col="col1", in2_col="col2", out_col="col3")
+    with pytest.raises(ColumnExistsError, match="column 'col3' already exists in the DataFrame"):
+        transformer.transform(dataframe)
+
+
+def test_column_lower_transformer_transform_exist_policy_warn(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = ColumnLower(in1_col="col1", in2_col="col2", out_col="col3", exist_policy="warn")
+    with pytest.warns(
+        ColumnExistsWarning,
+        match="column 'col3' already exists in the DataFrame and will be overwritten",
+    ):
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [5, 4, 3, 2, 1],
+                "col3": [True, True, False, False, False],
+            },
+            schema={"col1": pl.Int64, "col2": pl.Int64, "col3": pl.Boolean},
+        ),
+    )
+
+
+def test_column_lower_transformer_transform_missing_policy_ignore(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = ColumnLower(
+        in1_col="col",
+        in2_col="col2",
+        out_col="out",
+        missing_policy="ignore",
+    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [5, 4, 3, 2, 1],
+                "col3": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Int64, "col2": pl.Int64, "col3": pl.String},
+        ),
+    )
+
+
+def test_column_lower_transformer_transform_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = ColumnLower(in1_col="col", in2_col="col2", out_col="out")
+    with pytest.raises(ColumnNotFoundError, match="column 'col' is missing in the DataFrame"):
+        transformer.transform(dataframe)
+
+
+def test_column_lower_transformer_transform_missing_policy_warn(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = ColumnLower(
         in1_col="col",
         in2_col="col2",
         out_col="out",
