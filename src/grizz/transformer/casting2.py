a6aa48f84dee73ae7e1cce7fdd7ea207ac5f2ba1
Thibaut Durand <durand.tibo@gmail.com>
2025-01-19 15:07:27 -0800

    Add `InplaceCastTransformer` (#573)

--- src/grizz/transformer/casting2.py
+++ src/grizz/transformer/casting2.py
@@ -3,7 +3,7 @@ a new data type."""
 
 from __future__ import annotations
 
-__all__ = ["CastTransformer"]
+__all__ = ["CastTransformer", "InplaceCastTransformer"]
 
 import logging
 from typing import TYPE_CHECKING, Any
@@ -28,9 +28,19 @@ class CastTransformer(BaseInNOutNTransformer):
         columns: The columns to convert. ``None`` means all the
             columns.
         dtype: The target data type.
+        prefix: The column name prefix for the output columns.
+        suffix: The column name suffix for the output columns.
         exclude_columns: The columns to exclude from the input
             ``columns``. If any column is not found, it will be ignored
             during the filtering process.
+        exist_policy: The policy on how to handle existing columns.
+            The following options are available: ``'ignore'``,
+            ``'warn'``, and ``'raise'``. If ``'raise'``, an exception
+            is raised if at least one column already exist.
+            If ``'warn'``, a warning is raised if at least one column
+            already exist and the existing columns are overwritten.
+            If ``'ignore'``, the existing columns are overwritten and
+            no warning message appears.
         missing_policy: The policy on how to handle missing columns.
             The following options are available: ``'ignore'``,
             ``'warn'``, and ``'raise'``. If ``'raise'``, an exception
@@ -127,3 +137,101 @@ class CastTransformer(BaseInNOutNTransformer):
 
     def _cast(self, frame: pl.DataFrame, columns: Sequence[str]) -> pl.DataFrame:
         return frame.select(cs.by_name(columns).cast(self._dtype, **self._kwargs))
+
+
+class InplaceCastTransformer(CastTransformer):
+    r"""Implement a transformer to convert some columns to a new data
+    type.
+
+    ``InplaceCastTransformer`` is a specific implementation of
+    ``CastTransformer`` that performs the transformation in-place.
+
+    Args:
+        columns: The columns to convert. ``None`` means all the
+            columns.
+        dtype: The target data type.
+        exclude_columns: The columns to exclude from the input
+            ``columns``. If any column is not found, it will be ignored
+            during the filtering process.
+        missing_policy: The policy on how to handle missing columns.
+            The following options are available: ``'ignore'``,
+            ``'warn'``, and ``'raise'``. If ``'raise'``, an exception
+            is raised if at least one column is missing.
+            If ``'warn'``, a warning is raised if at least one column
+            is missing and the missing columns are ignored.
+            If ``'ignore'``, the missing columns are ignored and
+            no warning message appears.
+        **kwargs: The keyword arguments for ``cast``.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import polars as pl
+    >>> from grizz.transformer import InplaceCast
+    >>> transformer = InplaceCast(columns=["col1", "col3"], dtype=pl.Int32)
+    >>> transformer
+    InplaceCastTransformer(columns=('col1', 'col3'), exclude_columns=(), missing_policy='raise', dtype=Int32)
+    >>> frame = pl.DataFrame(
+    ...     {
+    ...         "col1": [1, 2, 3, 4, 5],
+    ...         "col2": ["1", "2", "3", "4", "5"],
+    ...         "col3": ["1", "2", "3", "4", "5"],
+    ...         "col4": ["a", "b", "c", "d", "e"],
+    ...     }
+    ... )
+    >>> frame
+    shape: (5, 4)
+    ┌──────┬──────┬──────┬──────┐
+    │ col1 ┆ col2 ┆ col3 ┆ col4 │
+    │ ---  ┆ ---  ┆ ---  ┆ ---  │
+    │ i64  ┆ str  ┆ str  ┆ str  │
+    ╞══════╪══════╪══════╪══════╡
+    │ 1    ┆ 1    ┆ 1    ┆ a    │
+    │ 2    ┆ 2    ┆ 2    ┆ b    │
+    │ 3    ┆ 3    ┆ 3    ┆ c    │
+    │ 4    ┆ 4    ┆ 4    ┆ d    │
+    │ 5    ┆ 5    ┆ 5    ┆ e    │
+    └──────┴──────┴──────┴──────┘
+    >>> out = transformer.transform(frame)
+    >>> out
+    shape: (5, 4)
+    ┌──────┬──────┬──────┬──────┐
+    │ col1 ┆ col2 ┆ col3 ┆ col4 │
+    │ ---  ┆ ---  ┆ ---  ┆ ---  │
+    │ i32  ┆ str  ┆ i32  ┆ str  │
+    ╞══════╪══════╪══════╪══════╡
+    │ 1    ┆ 1    ┆ 1    ┆ a    │
+    │ 2    ┆ 2    ┆ 2    ┆ b    │
+    │ 3    ┆ 3    ┆ 3    ┆ c    │
+    │ 4    ┆ 4    ┆ 4    ┆ d    │
+    │ 5    ┆ 5    ┆ 5    ┆ e    │
+    └──────┴──────┴──────┴──────┘
+
+    ```
+    """
+
+    def __init__(
+        self,
+        columns: Sequence[str] | None,
+        dtype: type[pl.DataType],
+        exclude_columns: Sequence[str] = (),
+        missing_policy: str = "raise",
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            columns=columns,
+            prefix="",
+            suffix="",
+            exclude_columns=exclude_columns,
+            exist_policy="ignore",
+            missing_policy=missing_policy,
+            dtype=dtype,
+            **kwargs,
+        )
+
+    def get_args(self) -> dict:
+        args = super().get_args()
+        for key in ["prefix", "suffix", "exist_policy"]:
+            args.pop(key)
+        return args
--- tests/unit/transformer/test_casting2.py
+++ tests/unit/transformer/test_casting2.py
@@ -14,7 +14,7 @@ from grizz.exceptions import (
     ColumnNotFoundError,
     ColumnNotFoundWarning,
 )
-from grizz.transformer.casting2 import CastTransformer
+from grizz.transformer.casting2 import CastTransformer, InplaceCastTransformer
 
 
 @pytest.fixture
@@ -261,6 +261,34 @@ def test_cast_transformer_transform(dataframe: pl.DataFrame) -> None:
     )
 
 
+def test_cast_transformer_transform_float32(dataframe: pl.DataFrame) -> None:
+    transformer = CastTransformer(
+        columns=["col1", "col3"], dtype=pl.Float32, prefix="", suffix="_out"
+    )
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": ["1", "2", "3", "4", "5"],
+                "col4": ["a", "b", "c", "d", "e"],
+                "col1_out": [1.0, 2.0, 3.0, 4.0, 5.0],
+                "col3_out": [1.0, 2.0, 3.0, 4.0, 5.0],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.String,
+                "col3": pl.String,
+                "col4": pl.String,
+                "col1_out": pl.Float32,
+                "col3_out": pl.Float32,
+            },
+        ),
+    )
+
+
 def test_cast_transformer_transform_exclude_columns(
     dataframe: pl.DataFrame,
 ) -> None:
@@ -426,5 +454,254 @@ def test_cast_transformer_transform_missing_policy_warn(
                 "col1_out": pl.Int32,
                 "col3_out": pl.Int32,
             },
+        ),
+    )
+
+
+############################################
+#     Tests for InplaceCastTransformer     #
+############################################
+
+
+def test_inplace_cast_transformer_repr() -> None:
+    assert repr(InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32)) == (
+        "InplaceCastTransformer(columns=('col1', 'col3'), exclude_columns=(), "
+        "missing_policy='raise', dtype=Int32)"
+    )
+
+
+def test_inplace_cast_transformer_str() -> None:
+    assert str(InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32)) == (
+        "InplaceCastTransformer(columns=('col1', 'col3'), exclude_columns=(), "
+        "missing_policy='raise', dtype=Int32)"
+    )
+
+
+def test_inplace_cast_transformer_equal_true() -> None:
+    assert InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32).equal(
+        InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32)
+    )
+
+
+def test_inplace_cast_transformer_equal_false_different_columns() -> None:
+    assert not InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32).equal(
+        InplaceCastTransformer(columns=["col1", "col2", "col3"], dtype=pl.Int32)
+    )
+
+
+def test_inplace_cast_transformer_equal_false_different_dtype() -> None:
+    assert not InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32).equal(
+        InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int64)
+    )
+
+
+def test_inplace_cast_transformer_equal_false_different_exclude_columns() -> None:
+    assert not InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32).equal(
+        InplaceCastTransformer(
+            columns=["col1", "col3"],
+            dtype=pl.Int32,
+            exclude_columns=["col4"],
+        )
+    )
+
+
+def test_inplace_cast_transformer_equal_false_different_missing_policy() -> None:
+    assert not InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32).equal(
+        InplaceCastTransformer(
+            columns=["col1", "col3"],
+            dtype=pl.Int32,
+            missing_policy="warn",
+        )
+    )
+
+
+def test_inplace_cast_transformer_equal_false_different_kwargs() -> None:
+    assert not InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32).equal(
+        InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32, strict=True)
+    )
+
+
+def test_inplace_cast_transformer_equal_false_different_type() -> None:
+    assert not InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32).equal(42)
+
+
+def test_inplace_cast_transformer_get_args() -> None:
+    assert objects_are_equal(
+        InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32, strict=True).get_args(),
+        {
+            "columns": ("col1", "col3"),
+            "exclude_columns": (),
+            "missing_policy": "raise",
+            "dtype": pl.Int32,
+            "strict": True,
+        },
+    )
+
+
+def test_inplace_cast_transformer_fit(
+    dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32)
+    with caplog.at_level(logging.INFO):
+        transformer.fit(dataframe)
+    assert caplog.messages[0].startswith(
+        "Skipping 'InplaceCastTransformer.fit' as there are no parameters available to fit"
+    )
+
+
+def test_inplace_cast_transformer_fit_missing_policy_ignore(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceCastTransformer(
+        columns=["col1", "col3", "col5"],
+        dtype=pl.Float32,
+        missing_policy="ignore",
+    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        transformer.fit(dataframe)
+
+
+def test_inplace_cast_transformer_fit_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceCastTransformer(columns=["col1", "col3", "col5"], dtype=pl.Float32)
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
+        transformer.fit(dataframe)
+
+
+def test_inplace_cast_transformer_fit_missing_policy_warn(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceCastTransformer(
+        columns=["col1", "col3", "col5"],
+        dtype=pl.Float32,
+        missing_policy="warn",
+    )
+    with pytest.warns(
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
+    ):
+        transformer.fit(dataframe)
+
+
+def test_inplace_cast_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32)
+    out = transformer.fit_transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [1, 2, 3, 4, 5],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Int32, "col2": pl.String, "col3": pl.Int32, "col4": pl.String},
+        ),
+    )
+
+
+def test_inplace_cast_transformer_transform(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Int32)
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [1, 2, 3, 4, 5],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Int32, "col2": pl.String, "col3": pl.Int32, "col4": pl.String},
+        ),
+    )
+
+
+def test_inplace_cast_transformer_transform_float32(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceCastTransformer(columns=["col1", "col3"], dtype=pl.Float32)
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1.0, 2.0, 3.0, 4.0, 5.0],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [1.0, 2.0, 3.0, 4.0, 5.0],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Float32, "col2": pl.String, "col3": pl.Float32, "col4": pl.String},
+        ),
+    )
+
+
+def test_inplace_cast_transformer_transform_exclude_columns(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceCastTransformer(
+        columns=None, dtype=pl.Int32, exclude_columns=["col2", "col4"]
+    )
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [1, 2, 3, 4, 5],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Int32, "col2": pl.String, "col3": pl.Int32, "col4": pl.String},
+        ),
+    )
+
+
+def test_inplace_cast_transformer_transform_missing_policy_ignore(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceCastTransformer(
+        columns=["col1", "col3", "col5"], dtype=pl.Int32, missing_policy="ignore"
+    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [1, 2, 3, 4, 5],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Int32, "col2": pl.String, "col3": pl.Int32, "col4": pl.String},
+        ),
+    )
+
+
+def test_inplace_cast_transformer_transform_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceCastTransformer(columns=["col1", "col3", "col5"], dtype=pl.Int32)
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
+        transformer.transform(dataframe)
+
+
+def test_inplace_cast_transformer_transform_missing_policy_warn(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceCastTransformer(
+        columns=["col1", "col3", "col5"], dtype=pl.Int32, missing_policy="warn"
+    )
+    with pytest.warns(
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
+    ):
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [1, 2, 3, 4, 5],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Int32, "col2": pl.String, "col3": pl.Int32, "col4": pl.String},
         ),
     )
