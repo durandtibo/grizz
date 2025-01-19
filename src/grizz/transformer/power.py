f1aba3c40cd0ff8b8fa86460737ff28e0f984c50
Thibaut Durand <durand.tibo@gmail.com>
2025-01-18 18:58:43 -0800

    Update `PowerTransformer`

--- src/grizz/transformer/power.py
+++ src/grizz/transformer/power.py
@@ -10,8 +10,7 @@ from typing import TYPE_CHECKING, Any
 
 import polars as pl
 
-from grizz.transformer.columns import BaseInNTransformer
-from grizz.utils.column import check_column_exist_policy, check_existing_columns
+from grizz.transformer.columns import BaseInNOutNTransformer
 from grizz.utils.imports import check_sklearn, is_sklearn_available
 from grizz.utils.null import propagate_nulls
 
@@ -24,7 +23,7 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-class PowerTransformer(BaseInNTransformer):
+class PowerTransformer(BaseInNOutNTransformer):
     r"""Implement a transformer to apply a power transform featurewise to
     make data more Gaussian-like.
 
@@ -66,7 +65,7 @@ class PowerTransformer(BaseInNTransformer):
     >>> from grizz.transformer import PowerTransformer
     >>> transformer = PowerTransformer(columns=["col1", "col3"], prefix="", suffix="_out")
     >>> transformer
-    PowerTransformer(columns=('col1', 'col3'), exclude_columns=(), missing_policy='raise', exist_policy='raise', propagate_nulls=True, prefix='', suffix='_out')
+    PowerTransformer(columns=('col1', 'col3'), exclude_columns=(), exist_policy='raise', missing_policy='raise', prefix='', suffix='_out', propagate_nulls=True)
     >>> frame = pl.DataFrame(
     ...     {
     ...         "col1": [0, 1, 2, 3, 4, 5],
@@ -122,31 +121,20 @@ class PowerTransformer(BaseInNTransformer):
     ) -> None:
         super().__init__(
             columns=columns,
+            prefix=prefix,
+            suffix=suffix,
             exclude_columns=exclude_columns,
+            exist_policy=exist_policy,
             missing_policy=missing_policy,
         )
-        self._prefix = prefix
-        self._suffix = suffix
         self._propagate_nulls = propagate_nulls
-
-        check_column_exist_policy(exist_policy)
-        self._exist_policy = exist_policy
 
         check_sklearn()
         self._transformer = sklearn.preprocessing.PowerTransformer(**kwargs)
         self._kwargs = kwargs
 
     def get_args(self) -> dict:
-        return (
-            super().get_args()
-            | {
-                "exist_policy": self._exist_policy,
-                "propagate_nulls": self._propagate_nulls,
-                "prefix": self._prefix,
-                "suffix": self._suffix,
-            }
-            | self._kwargs
-        )
+        return super().get_args() | {"propagate_nulls": self._propagate_nulls} | self._kwargs
 
     def _fit(self, frame: pl.DataFrame) -> None:
         logger.info(
@@ -156,7 +144,6 @@ class PowerTransformer(BaseInNTransformer):
         self._transformer.fit(frame.select(columns).to_numpy())
 
     def _transform(self, frame: pl.DataFrame) -> pl.DataFrame:
-        self._check_output_columns(frame)
         logger.info(
             f"Applying the power transformation on {len(self.find_columns(frame)):,} columns | "
             f"prefix={self._prefix!r} | suffix={self._suffix!r}"
@@ -165,19 +152,7 @@ class PowerTransformer(BaseInNTransformer):
         data = frame.select(columns)
 
         x = self._transformer.transform(data.to_numpy())
-        data_out = pl.from_numpy(x, schema=data.columns)
+        out = pl.from_numpy(x, schema=data.columns)
         if self._propagate_nulls:
-            data_out = propagate_nulls(data_out, data)
-        return frame.with_columns(data_out.rename(lambda col: f"{self._prefix}{col}{self._suffix}"))
-
-    def _check_output_columns(self, frame: pl.DataFrame) -> None:
-        r"""Check if the output columns already exist.
-
-        Args:
-            frame: The input DataFrame to check.
-        """
-        check_existing_columns(
-            frame,
-            columns=[f"{self._prefix}{col}{self._suffix}" for col in self.find_columns(frame)],
-            exist_policy=self._exist_policy,
-        )
+            out = propagate_nulls(out, data)
+        return out
