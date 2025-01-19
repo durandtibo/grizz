9680cb9bbb5dbff75c7f5ab28d0672031572f763
Thibaut Durand <durand.tibo@gmail.com>
2025-01-18 18:45:25 -0800

    Update `RobustScalerTransformer` (#542)

--- tests/unit/transformer/test_robust_scaler.py
+++ tests/unit/transformer/test_robust_scaler.py
@@ -47,8 +47,8 @@ def dataframe() -> pl.DataFrame:
 def test_robust_scaler_transformer_repr() -> None:
     assert repr(RobustScaler(columns=["col1", "col3"], prefix="", suffix="_out")) == (
         "RobustScalerTransformer(columns=('col1', 'col3'), exclude_columns=(), "
-        "missing_policy='raise', exist_policy='raise', propagate_nulls=True, prefix='', "
-        "suffix='_out')"
+        "exist_policy='raise', missing_policy='raise', prefix='', suffix='_out', "
+        "propagate_nulls=True)"
     )
 
 
@@ -56,8 +56,8 @@ def test_robust_scaler_transformer_repr() -> None:
 def test_robust_scaler_transformer_str() -> None:
     assert str(RobustScaler(columns=["col1", "col3"], prefix="", suffix="_out")) == (
         "RobustScalerTransformer(columns=('col1', 'col3'), exclude_columns=(), "
-        "missing_policy='raise', exist_policy='raise', propagate_nulls=True, prefix='', "
-        "suffix='_out')"
+        "exist_policy='raise', missing_policy='raise', prefix='', suffix='_out', "
+        "propagate_nulls=True)"
     )
 
 
@@ -156,6 +156,15 @@ def test_robust_scaler_transformer_fit(dataframe: pl.DataFrame) -> None:
 
 
 @sklearn_available
+def test_robust_scaler_transformer_fit_exclude_columns(dataframe: pl.DataFrame) -> None:
+    transformer = RobustScaler(
+        columns=None, prefix="", suffix="_out", exclude_columns=["col2", "col4"]
+    )
+    transformer.fit(dataframe)
+    assert transformer._scaler.n_features_in_ == 2
+
+
+@sklearn_available
 def test_robust_scaler_transformer_fit_missing_policy_ignore(
     dataframe: pl.DataFrame,
 ) -> None:
@@ -222,6 +231,36 @@ def test_robust_scaler_transformer_fit_transform(dataframe: pl.DataFrame) -> Non
 @sklearn_available
 def test_robust_scaler_transformer_transform(dataframe: pl.DataFrame) -> None:
     transformer = RobustScaler(columns=["col1", "col3"], prefix="", suffix="_out")
+    transformer._scaler.fit(np.array([[1, 10], [2, 20], [3, 30], [4, 40], [5, 50]]))
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [10, 20, 30, 40, 50],
+                "col4": ["a", "b", "c", "d", "e"],
+                "col1_out": [-1.0, -0.5, 0.0, 0.5, 1.0],
+                "col3_out": [-1.0, -0.5, 0.0, 0.5, 1.0],
+            },
+            schema={
+                "col1": pl.Int64,
+                "col2": pl.Float32,
+                "col3": pl.Int64,
+                "col4": pl.String,
+                "col1_out": pl.Float64,
+                "col3_out": pl.Float64,
+            },
+        ),
+    )
+
+
+@sklearn_available
+def test_robust_scaler_transformer_transform_exclude_columns(dataframe: pl.DataFrame) -> None:
+    transformer = RobustScaler(
+        columns=None, prefix="", suffix="_out", exclude_columns=["col2", "col4"]
+    )
     transformer._scaler.fit(np.array([[1, 10], [2, 20], [3, 30], [4, 40], [5, 50]]))
     out = transformer.transform(dataframe)
     assert_frame_equal(
