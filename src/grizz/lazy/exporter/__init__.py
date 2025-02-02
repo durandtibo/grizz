7fd62cf212a261d0b0233a71bdd4f430070f2d2b
Thibaut Durand <durand.tibo@gmail.com>
2025-02-02 10:40:30 -0800

    Add `ParquetExporter` for `LazyFrame` (#603)

--- src/grizz/lazy/exporter/__init__.py
+++ src/grizz/lazy/exporter/__init__.py
@@ -2,6 +2,7 @@ r"""Contain LazyFrame exporters."""
 
 from __future__ import annotations
 
-__all__ = ["BaseExporter", "is_exporter_config", "setup_exporter"]
+__all__ = ["BaseExporter", "ParquetExporter", "is_exporter_config", "setup_exporter"]
 
 from grizz.lazy.exporter.base import BaseExporter, is_exporter_config, setup_exporter
+from grizz.lazy.exporter.parquet import ParquetExporter
