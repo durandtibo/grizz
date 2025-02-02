8a9a841c833c8c778f82857f34f4402571851359
Thibaut Durand <durand.tibo@gmail.com>
2025-02-02 10:24:35 -0800

    Add `CsvFileIngestor` (#601)

--- src/grizz/ingestor/__init__.py
+++ src/grizz/ingestor/__init__.py
@@ -6,6 +6,7 @@ __all__ = [
     "BaseIngestor",
     "CacheIngestor",
     "ClickHouseArrowIngestor",
+    "CsvFileIngestor",
     "CsvIngestor",
     "Ingestor",
     "JoinIngestor",
@@ -19,7 +20,7 @@ __all__ = [
 from grizz.ingestor.base import BaseIngestor, is_ingestor_config, setup_ingestor
 from grizz.ingestor.cache import CacheIngestor
 from grizz.ingestor.clickhouse import ClickHouseArrowIngestor
-from grizz.ingestor.csv import CsvIngestor
+from grizz.ingestor.csv import CsvFileIngestor, CsvIngestor
 from grizz.ingestor.join import JoinIngestor
 from grizz.ingestor.parquet import ParquetFileIngestor, ParquetIngestor
 from grizz.ingestor.transform import TransformIngestor
