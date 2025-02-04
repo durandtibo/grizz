e96434fad357214b4ed60d338bdf7a4832d86499
Thibaut Durand <durand.tibo@gmail.com>
2025-02-03 19:19:30 -0800

    Add `InplaceQuantileTransformer` (#618)

--- src/grizz/transformer/quantile.py
+++ src/grizz/transformer/quantile.py
@@ -3,7 +3,7 @@ transformation."""
 
 from __future__ import annotations
 
-__all__ = ["QuantileTransformer"]
+__all__ = ["InplaceQuantileTransformer", "QuantileTransformer"]
 
 import logging
 from typing import TYPE_CHECKING, Any
@@ -155,3 +155,103 @@ class QuantileTransformer(BaseInNOutNTransformer):
         if self._propagate_nulls:
             out = propagate_nulls(out, data)
         return out
+
+
+class InplaceQuantileTransformer(QuantileTransformer):
+    r"""Implement a transformer to scale each column using statistics
+    that are robust to outliers.
+
+    Args:
+        columns: The columns to scale. ``None`` means all the
+            columns.
+        exclude_columns: The columns to exclude from the input
+            ``columns``. If any column is not found, it will be ignored
+            during the filtering process.
+        propagate_nulls: If set to ``True``, the ``None`` values are
+            propagated after the transformation. If ``False``, the
+            ``None`` values are replaced by NaNs.
+        missing_policy: The policy on how to handle missing columns.
+            The following options are available: ``'ignore'``,
+            ``'warn'``, and ``'raise'``. If ``'raise'``, an exception
+            is raised if at least one column is missing.
+            If ``'warn'``, a warning is raised if at least one column
+            is missing and the missing columns are ignored.
+            If ``'ignore'``, the missing columns are ignored and
+            no warning message appears.
+        **kwargs: Additional arguments passed to
+            ``sklearn.preprocessing.QuantileTransformer``.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import polars as pl
+    >>> from grizz.transformer import InplaceQuantileTransformer
+    >>> transformer = InplaceQuantileTransformer(columns=["col1", "col3"])
+    >>> transformer
+    InplaceQuantileTransformer(columns=('col1', 'col3'), exclude_columns=(), missing_policy='raise', propagate_nulls=True)
+    >>> frame = pl.DataFrame(
+    ...     {
+    ...         "col1": [0, 1, 2, 3, 4, 5],
+    ...         "col2": ["0", "1", "2", "3", "4", "5"],
+    ...         "col3": [0, 10, 20, 30, 40, 50],
+    ...         "col4": ["a", "b", "c", "d", "e", "f"],
+    ...     }
+    ... )
+    >>> frame
+    shape: (6, 4)
+    ┌──────┬──────┬──────┬──────┐
+    │ col1 ┆ col2 ┆ col3 ┆ col4 │
+    │ ---  ┆ ---  ┆ ---  ┆ ---  │
+    │ i64  ┆ str  ┆ i64  ┆ str  │
+    ╞══════╪══════╪══════╪══════╡
+    │ 0    ┆ 0    ┆ 0    ┆ a    │
+    │ 1    ┆ 1    ┆ 10   ┆ b    │
+    │ 2    ┆ 2    ┆ 20   ┆ c    │
+    │ 3    ┆ 3    ┆ 30   ┆ d    │
+    │ 4    ┆ 4    ┆ 40   ┆ e    │
+    │ 5    ┆ 5    ┆ 50   ┆ f    │
+    └──────┴──────┴──────┴──────┘
+    >>> out = transformer.fit_transform(frame)
+    >>> out
+    shape: (6, 4)
+    ┌──────┬──────┬──────┬──────┐
+    │ col1 ┆ col2 ┆ col3 ┆ col4 │
+    │ ---  ┆ ---  ┆ ---  ┆ ---  │
+    │ f64  ┆ str  ┆ f64  ┆ str  │
+    ╞══════╪══════╪══════╪══════╡
+    │ 0.0  ┆ 0    ┆ 0.0  ┆ a    │
+    │ 0.2  ┆ 1    ┆ 0.2  ┆ b    │
+    │ 0.4  ┆ 2    ┆ 0.4  ┆ c    │
+    │ 0.6  ┆ 3    ┆ 0.6  ┆ d    │
+    │ 0.8  ┆ 4    ┆ 0.8  ┆ e    │
+    │ 1.0  ┆ 5    ┆ 1.0  ┆ f    │
+    └──────┴──────┴──────┴──────┘
+
+    ```
+    """
+
+    def __init__(
+        self,
+        columns: Sequence[str] | None,
+        exclude_columns: Sequence[str] = (),
+        propagate_nulls: bool = True,
+        missing_policy: str = "raise",
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            columns=columns,
+            prefix="",
+            suffix="",
+            exclude_columns=exclude_columns,
+            exist_policy="ignore",
+            missing_policy=missing_policy,
+            propagate_nulls=propagate_nulls,
+            **kwargs,
+        )
+
+    def get_args(self) -> dict:
+        args = super().get_args()
+        for key in ["prefix", "suffix", "exist_policy"]:
+            args.pop(key)
+        return args
--- tests/unit/transformer/test_quantile.py
+++ tests/unit/transformer/test_quantile.py
@@ -16,7 +16,7 @@ from grizz.exceptions import (
     ColumnNotFoundWarning,
 )
 from grizz.testing.fixture import sklearn_available
-from grizz.transformer import QuantileTransformer
+from grizz.transformer import InplaceQuantileTransformer, QuantileTransformer
 from grizz.utils.imports import is_sklearn_available
 
 if is_numpy_available():
@@ -650,3 +650,409 @@ def test_quantile_transformer_no_sklearn() -> None:
         pytest.raises(RuntimeError, match="'sklearn' package is required but not installed."),
     ):
         QuantileTransformer(columns=["col1", "col3"], prefix="", suffix="_out")
+
+
+################################################
+#     Tests for InplaceQuantileTransformer     #
+################################################
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_repr() -> None:
+    assert repr(InplaceQuantileTransformer(columns=["col1", "col3"])) == (
+        "InplaceQuantileTransformer(columns=('col1', 'col3'), exclude_columns=(), "
+        "missing_policy='raise', propagate_nulls=True)"
+    )
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_str() -> None:
+    assert str(InplaceQuantileTransformer(columns=["col1", "col3"])) == (
+        "InplaceQuantileTransformer(columns=('col1', 'col3'), exclude_columns=(), "
+        "missing_policy='raise', propagate_nulls=True)"
+    )
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_equal_true() -> None:
+    assert InplaceQuantileTransformer(columns=["col1", "col3"]).equal(
+        InplaceQuantileTransformer(columns=["col1", "col3"])
+    )
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_equal_false_different_columns() -> None:
+    assert not InplaceQuantileTransformer(columns=["col1", "col3"]).equal(
+        InplaceQuantileTransformer(columns=["col1", "col2", "col3"])
+    )
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_equal_false_different_exclude_columns() -> None:
+    assert not InplaceQuantileTransformer(columns=["col1", "col3"]).equal(
+        InplaceQuantileTransformer(columns=["col1", "col3"], exclude_columns=["col4"])
+    )
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_equal_false_different_missing_policy() -> None:
+    assert not InplaceQuantileTransformer(columns=["col1", "col3"]).equal(
+        InplaceQuantileTransformer(columns=["col1", "col3"], missing_policy="warn")
+    )
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_equal_false_different_propagate_nulls() -> None:
+    assert not InplaceQuantileTransformer(columns=["col1", "col3"]).equal(
+        InplaceQuantileTransformer(columns=["col1", "col3"], propagate_nulls=False)
+    )
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_equal_false_different_kwargs() -> None:
+    assert not InplaceQuantileTransformer(columns=["col1", "col3"]).equal(
+        InplaceQuantileTransformer(columns=["col1", "col3"], n_quantiles=100)
+    )
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_equal_false_different_type() -> None:
+    assert not InplaceQuantileTransformer(columns=["col1", "col3"]).equal(42)
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_get_args() -> None:
+    assert objects_are_equal(
+        InplaceQuantileTransformer(columns=["col1", "col3"], n_quantiles=100).get_args(),
+        {
+            "columns": ("col1", "col3"),
+            "exclude_columns": (),
+            "missing_policy": "raise",
+            "propagate_nulls": True,
+            "n_quantiles": 100,
+        },
+    )
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_fit(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceQuantileTransformer(columns=["col1", "col3"])
+    transformer.fit(dataframe)
+    assert transformer._transformer.n_features_in_ == 2
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_fit_exclude_columns(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceQuantileTransformer(columns=None, exclude_columns=["col2", "col4"])
+    transformer.fit(dataframe)
+    assert transformer._transformer.n_features_in_ == 2
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_fit_missing_policy_ignore(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceQuantileTransformer(
+        columns=["col1", "col3", "col5"], missing_policy="ignore", n_quantiles=5
+    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        transformer.fit(dataframe)
+    assert transformer._transformer.n_features_in_ == 2
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_fit_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceQuantileTransformer(columns=["col1", "col3", "col5"])
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
+        transformer.fit(dataframe)
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_fit_missing_policy_warn(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceQuantileTransformer(
+        columns=["col1", "col3", "col5"], missing_policy="warn"
+    )
+    with pytest.warns(
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
+    ):
+        transformer.fit(dataframe)
+    assert transformer._transformer.n_features_in_ == 2
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceQuantileTransformer(columns=["col1", "col3"])
+    out = transformer.fit_transform(dataframe)
+    assert transformer._transformer.n_features_in_ == 2
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [0.0, 0.25, 0.5, 0.75, 1.0],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [0.0, 0.25, 0.5, 0.75, 1.0],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Float64, "col2": pl.Float32, "col3": pl.Float64, "col4": pl.String},
+        ),
+    )
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_transform(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceQuantileTransformer(columns=["col1", "col3"])
+    transformer._transformer.fit(np.array([[1, 10], [2, 20], [3, 30], [4, 40], [5, 50]]))
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [0.0, 0.25, 0.5, 0.75, 1.0],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [0.0, 0.25, 0.5, 0.75, 1.0],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Float64, "col2": pl.Float32, "col3": pl.Float64, "col4": pl.String},
+        ),
+    )
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_transform_exclude_columns(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceQuantileTransformer(columns=None, exclude_columns=["col2", "col4"])
+    transformer._transformer.fit(np.array([[1, 10], [2, 20], [3, 30], [4, 40], [5, 50]]))
+    out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [0.0, 0.25, 0.5, 0.75, 1.0],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [0.0, 0.25, 0.5, 0.75, 1.0],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Float64, "col2": pl.Float32, "col3": pl.Float64, "col4": pl.String},
+        ),
+    )
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_transform_propagate_nulls_true() -> None:
+    frame = pl.DataFrame(
+        {
+            "col1": [1, 2, 3, 4, 5, None, 1, None, float("nan"), 1, float("nan")],
+            "col2": [-1.0, -2.0, -3.0, -4.0, -5.0, 1, None, None, 1, float("nan"), float("nan")],
+            "col3": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
+        },
+        schema={"col1": pl.Float32, "col2": pl.Float32, "col3": pl.Int64},
+    )
+    transformer = InplaceQuantileTransformer(columns=["col1", "col2", "col3"])
+    transformer._transformer.fit(
+        np.array([[1, -1, 10], [2, -2, 20], [3, -3, 30], [4, -4, 40], [5, -5, 50]])
+    )
+    out = transformer.transform(frame)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [
+                    0.0,
+                    0.25,
+                    0.5,
+                    0.75,
+                    1.0,
+                    None,
+                    0.0,
+                    None,
+                    float("nan"),
+                    0.0,
+                    float("nan"),
+                ],
+                "col2": [
+                    1.0,
+                    0.75,
+                    0.5,
+                    0.25,
+                    0.0,
+                    1.0,
+                    None,
+                    None,
+                    1.0,
+                    float("nan"),
+                    float("nan"),
+                ],
+                "col3": [0.0, 0.25, 0.5, 0.75, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            },
+            schema={"col1": pl.Float64, "col2": pl.Float64, "col3": pl.Float64},
+        ),
+    )
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_transform_propagate_nulls_false() -> None:
+    frame = pl.DataFrame(
+        {
+            "col1": [1, 2, 3, 4, 5, None, 1, None, float("nan"), 1, float("nan")],
+            "col2": [-1.0, -2.0, -3.0, -4.0, -5.0, 1, None, None, 1, float("nan"), float("nan")],
+            "col3": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
+        },
+        schema={"col1": pl.Float32, "col2": pl.Float32, "col3": pl.Int64},
+    )
+    transformer = InplaceQuantileTransformer(
+        columns=["col1", "col2", "col3"], propagate_nulls=False
+    )
+    transformer._transformer.fit(
+        np.array([[1, -1, 10], [2, -2, 20], [3, -3, 30], [4, -4, 40], [5, -5, 50]])
+    )
+    out = transformer.transform(frame)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [
+                    0.0,
+                    0.25,
+                    0.5,
+                    0.75,
+                    1.0,
+                    float("nan"),
+                    0.0,
+                    float("nan"),
+                    float("nan"),
+                    0.0,
+                    float("nan"),
+                ],
+                "col2": [
+                    1.0,
+                    0.75,
+                    0.5,
+                    0.25,
+                    0.0,
+                    1.0,
+                    float("nan"),
+                    float("nan"),
+                    1.0,
+                    float("nan"),
+                    float("nan"),
+                ],
+                "col3": [0.0, 0.25, 0.5, 0.75, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            },
+            schema={"col1": pl.Float64, "col2": pl.Float64, "col3": pl.Float64},
+        ),
+    )
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_transform_not_fitted(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceQuantileTransformer(columns=["col1", "col3"])
+    with pytest.raises(
+        sklearn.exceptions.NotFittedError,
+        match="This QuantileTransformer instance is not fitted yet.",
+    ):
+        transformer.transform(dataframe)
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_transform_missing_policy_ignore(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceQuantileTransformer(
+        columns=["col1", "col3", "col5"], missing_policy="ignore"
+    )
+    transformer._transformer.fit(np.array([[1, 10], [2, 20], [3, 30], [4, 40], [5, 50]]))
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [0.0, 0.25, 0.5, 0.75, 1.0],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [0.0, 0.25, 0.5, 0.75, 1.0],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Float64, "col2": pl.Float32, "col3": pl.Float64, "col4": pl.String},
+        ),
+    )
+
+
+@sklearn_available
+def test_inplace_quantile_transformer_transform_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceQuantileTransformer(columns=["col1", "col3", "col5"])
+    with pytest.raises(ColumnNotFoundError, match="1 column is missing in the DataFrame:"):
+        transformer.transform(dataframe)
+
+
+@sklearn_available
+@pytest.mark.filterwarnings(
+    "ignore:n_quantiles .* is greater than the total number of samples .*:UserWarning"
+)
+def test_inplace_quantile_transformer_transform_missing_policy_warn(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceQuantileTransformer(
+        columns=["col1", "col3", "col5"], missing_policy="warn"
+    )
+    transformer._transformer.fit(np.array([[1, 10], [2, 20], [3, 30], [4, 40], [5, 50]]))
+    with pytest.warns(
+        ColumnNotFoundWarning, match="1 column is missing in the DataFrame and will be ignored:"
+    ):
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [0.0, 0.25, 0.5, 0.75, 1.0],
+                "col2": [-1.0, -2.0, -3.0, -4.0, -5.0],
+                "col3": [0.0, 0.25, 0.5, 0.75, 1.0],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Float64, "col2": pl.Float32, "col3": pl.Float64, "col4": pl.String},
+        ),
+    )
+
+
+def test_inplace_quantile_transformer_no_sklearn() -> None:
+    with (
+        patch("grizz.utils.imports.is_sklearn_available", lambda: False),
+        pytest.raises(RuntimeError, match="'sklearn' package is required but not installed."),
+    ):
+        InplaceQuantileTransformer(columns=["col1", "col3"])
