c05424c5425128e85ea8f3fdce79de38a436b07a
Thibaut Durand <durand.tibo@gmail.com>
2025-02-04 00:14:15 -0800

    Add `InplaceLabelEncoderTransformer`

--- tests/unit/transformer/sklearn/test_label_encoder.py
+++ tests/unit/transformer/sklearn/test_label_encoder.py
@@ -15,7 +15,7 @@ from grizz.exceptions import (
     ColumnNotFoundWarning,
 )
 from grizz.testing.fixture import sklearn_available
-from grizz.transformer import LabelEncoder
+from grizz.transformer import InplaceLabelEncoder, LabelEncoder
 from grizz.utils.imports import is_sklearn_available
 
 if is_sklearn_available():
@@ -342,3 +342,207 @@ def test_label_encoder_transformer_no_sklearn() -> None:
         pytest.raises(RuntimeError, match="'sklearn' package is required but not installed."),
     ):
         LabelEncoder(in_col="in", out_col="out")
+
+
+####################################################
+#     Tests for InplaceLabelEncoderTransformer     #
+####################################################
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_repr() -> None:
+    assert repr(InplaceLabelEncoder(col="col")) == (
+        "InplaceLabelEncoderTransformer(col='col', missing_policy='raise')"
+    )
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_str() -> None:
+    assert str(InplaceLabelEncoder(col="col")) == (
+        "InplaceLabelEncoderTransformer(col='col', missing_policy='raise')"
+    )
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_equal_true() -> None:
+    assert InplaceLabelEncoder(col="col").equal(InplaceLabelEncoder(col="col"))
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_equal_false_different_col() -> None:
+    assert not InplaceLabelEncoder(col="col").equal(InplaceLabelEncoder(col="col3"))
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_equal_false_different_missing_policy() -> None:
+    assert not InplaceLabelEncoder(col="col").equal(
+        InplaceLabelEncoder(col="col", missing_policy="warn")
+    )
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_equal_false_different_type() -> None:
+    assert not InplaceLabelEncoder(col="col").equal(42)
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_get_args() -> None:
+    assert objects_are_equal(
+        InplaceLabelEncoder(col="col").get_args(),
+        {"col": "col", "missing_policy": "raise"},
+    )
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_fit(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceLabelEncoder(col="col1")
+    transformer.fit(dataframe)
+    assert objects_are_equal(list(transformer._encoder.classes_), ["amsterdam", "paris", "tokyo"])
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_fit_missing_policy_ignore(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceLabelEncoder(col="col", missing_policy="ignore")
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        transformer.fit(dataframe)
+    assert not hasattr(transformer._encoder, "classes_")
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_fit_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceLabelEncoder(col="col")
+    with pytest.raises(ColumnNotFoundError, match="column 'col' is missing in the DataFrame"):
+        transformer.fit(dataframe)
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_fit_missing_policy_warn(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceLabelEncoder(col="col", missing_policy="warn")
+    with pytest.warns(
+        ColumnNotFoundWarning, match="column 'col' is missing in the DataFrame and will be ignored"
+    ):
+        transformer.fit(dataframe)
+    assert not hasattr(transformer._encoder, "classes_")
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_fit_transform(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceLabelEncoder(col="col1")
+    out = transformer.fit_transform(dataframe)
+    assert objects_are_equal(list(transformer._encoder.classes_), ["amsterdam", "paris", "tokyo"])
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 1, 2, 0, 2],
+                "col2": [1, 2, 3, 4, 5],
+                "col3": ["1", "2", "3", "4", "5"],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={"col1": pl.Int64, "col2": pl.Int64, "col3": pl.String, "col4": pl.String},
+        ),
+    )
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_transform() -> None:
+    transformer = InplaceLabelEncoder(col="col1")
+    transformer._encoder.fit(["tokyo", "amsterdam", "paris", "tokyo"])
+
+    frame = pl.DataFrame(
+        {"col1": ["tokyo", "amsterdam", "paris", "tokyo"]}, schema={"col1": pl.String}
+    )
+    out = transformer.transform(frame)
+    assert_frame_equal(
+        out,
+        pl.DataFrame({"col1": [2, 0, 1, 2]}, schema={"col1": pl.Int64}),
+    )
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_transform_not_fitted(dataframe: pl.DataFrame) -> None:
+    transformer = InplaceLabelEncoder(col="col1")
+    with pytest.raises(
+        sklearn.exceptions.NotFittedError,
+        match="This LabelEncoder instance is not fitted yet.",
+    ):
+        transformer.transform(dataframe)
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_transform_missing_policy_ignore(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceLabelEncoder(col="col", missing_policy="ignore")
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": ["paris", "paris", "tokyo", "amsterdam", "tokyo"],
+                "col2": [1, 2, 3, 4, 5],
+                "col3": ["1", "2", "3", "4", "5"],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={
+                "col1": pl.String,
+                "col2": pl.Int64,
+                "col3": pl.String,
+                "col4": pl.String,
+            },
+        ),
+    )
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_transform_missing_policy_raise(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceLabelEncoder(col="col")
+    with pytest.raises(ColumnNotFoundError, match="column 'col' is missing in the DataFrame"):
+        transformer.transform(dataframe)
+
+
+@sklearn_available
+def test_inplace_label_encoder_transformer_transform_missing_policy_warn(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = InplaceLabelEncoder(col="col", missing_policy="warn")
+    with pytest.warns(
+        ColumnNotFoundWarning, match="column 'col' is missing in the DataFrame and will be ignored"
+    ):
+        out = transformer.transform(dataframe)
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
+            {
+                "col1": ["paris", "paris", "tokyo", "amsterdam", "tokyo"],
+                "col2": [1, 2, 3, 4, 5],
+                "col3": ["1", "2", "3", "4", "5"],
+                "col4": ["a", "b", "c", "d", "e"],
+            },
+            schema={
+                "col1": pl.String,
+                "col2": pl.Int64,
+                "col3": pl.String,
+                "col4": pl.String,
+            },
+        ),
+    )
+
+
+def test_inplace_label_encoder_transformer_no_sklearn() -> None:
+    with (
+        patch("grizz.utils.imports.is_sklearn_available", lambda: False),
+        pytest.raises(RuntimeError, match="'sklearn' package is required but not installed."),
+    ):
+        InplaceLabelEncoder(col="col")
