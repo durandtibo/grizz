27fe1c52d3e71e5f8416a0510a13207786bbd951
Thibaut Durand <durand.tibo@gmail.com>
2025-02-07 23:42:24 -0800

    Add `ConcatColumnsTransformer` for `LazyFrame`

--- src/grizz/lazy/transformer/__init__.py
+++ src/grizz/lazy/transformer/__init__.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 __all__ = [
     "BaseTransformer",
+    "ConcatColumns",
+    "ConcatColumnsTransformer",
     "DropNullRow",
     "DropNullRowTransformer",
     "SqlTransformer",
@@ -16,6 +18,8 @@ from grizz.lazy.transformer.base import (
     is_transformer_config,
     setup_transformer,
 )
+from grizz.lazy.transformer.concat import ConcatColumnsTransformer
+from grizz.lazy.transformer.concat import ConcatColumnsTransformer as ConcatColumns
 from grizz.lazy.transformer.null import DropNullRowTransformer
 from grizz.lazy.transformer.null import DropNullRowTransformer as DropNullRow
 from grizz.lazy.transformer.sql import SqlTransformer
